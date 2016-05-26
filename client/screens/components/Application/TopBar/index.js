b2cee0f14a25b3b1ec3cfe2de8371b2fa55c2d3c
paothegit <pao.tech1018@gmail.com>
2016-05-26 03:03:25 +0300

    rewrite TopBar component

--- client/screens/components/Application/TopBar/index.js
+++ client/screens/components/Application/TopBar/index.js
@@ -4,7 +4,7 @@ import { Nav, Navbar, NavItem } from 'react-bootstrap'
 
 import './styles.less'
 
-import LogoImg from 'assets/images/logo.png'
+import logoImg from 'assets/images/logo.png'
 import hfbImg from 'assets/images/social/hfb.png'
 import hlkImg from 'assets/images/social/hlk.png'
 import hgpImg from 'assets/images/social/hgp.png'
@@ -12,49 +12,90 @@ import httImg from 'assets/images/social/htt.png'
 import hinImg from 'assets/images/social/hin.png'
 import htpImg from 'assets/images/social/htp.png'
 
+const menuItems = [ {
+  text: 'Trial Search',
+  link: '/' // Need to define exactly since it'll have 'active' class always
+}, {
+  text: 'List Your Trials',
+  link: '/clinical-trial-patient-recruitment-patient-enrollment'
+}, {
+  text: 'Blog',
+  link: '/blog'
+}, {
+  text: 'About Us',
+  link: '/about'
+}, {
+  text: 'Contact',
+  link: '/contact'
+} ]
+
+const socialShares = [ {
+  name: 'facebook',
+  image: hfbImg,
+  link: 'https://www.facebook.com/sharer/sharer.php?=www.studykik.com&amp;u=https://studykik.com'
+}, {
+  name: 'linkedin',
+  image: hlkImg,
+  link: 'https://www.linkedin.com/shareArticle?mini=true&amp;url=https://studykik.com/&amp;title=Find%20Clinical%20Trials%20Near%20You&amp;summary=&amp;source='
+}, {
+  name: 'google',
+  image: hgpImg,
+  link: 'https://plus.google.com/share?url=[https://studykik.com]'
+}, {
+  name: 'twitter',
+  image: httImg,
+  link: 'https://twitter.com/home?status=[Find Clinical Trials Near You]+[https://studykik.com]'
+}, {
+  name: 'instagram',
+  image: hinImg,
+  link: 'https://instagram.com/studykik/#'
+}, {
+  name: 'pinterest',
+  image: htpImg,
+  link: 'https://pinterest.com/pin/create/bookmarklet/?media=https://studykik.com/wp-content/themes/twentythirteen/images/logo.png&amp;url=https://studykik.com&amp;is_video=false&amp;description=Find Clinical Trials Near You'
+} ]
+
 export default class TopBar extends React.Component {
   render () {
     return (
-      <div>
-        <Navbar className="navbar-studykik">
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/studies" className="logo">
-                <img src={LogoImg} />
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <NavItem eventKey={1} href="/landing" className="nav-item">TRIAL SEARCH</NavItem>
-              <NavItem eventKey={2} href="/clinical-trial-patient-recruitment-patient-enrollment" className="nav-item">LIST YOUR TRIALS</NavItem>
-              <NavItem eventKey={3} href="/blog" className="nav-item">BLOG</NavItem>
-              <NavItem eventKey={4} href="/about" className="nav-item">ABOUT US</NavItem>
-              <NavItem eventKey={5} href="/contact" className="nav-item">CONTACT</NavItem>
-            </Nav>
+      <Navbar className="navbar-studykik">
 
-            <Nav pullRight>
-              <NavItem eventKey={1} href="/login">Login</NavItem>
-            </Nav>
-            <ul className="head-social nav navbar-nav navbar-right">
-              <li><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?=www.studykik.com&amp;u=https://studykik.com">
-                <img src={hfbImg} alt="" height="32" width="32" />
-                </a></li>
-              <li><a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=https://studykik.com/&amp;title=Find%20Clinical%20Trials%20Near%20You&amp;summary=&amp;source=">
-                <img src={hlkImg} alt="" height="32" width="32" /></a></li>
-              <li><a target="_blank" href="https://plus.google.com/share?url=[https://studykik.com]">
-                <img src={hgpImg} alt="" height="32" width="32" /></a></li>
-              <li> <a target="_blank" href="https://twitter.com/home?status=[Find Clinical Trials Near You]+[https://studykik.com]">
-                <img src={httImg} alt="" height="32" width="32" /></a></li>
-              <li><a target="_blank" href="https://instagram.com/studykik/#">
-                <img src={hinImg} alt="" height="32" width="32" /></a></li>
-              <li><a target="_blank" href="https://pinterest.com/pin/create/bookmarklet/?media=https://studykik.com/wp-content/themes/twentythirteen/images/logo.png&amp;url=https://studykik.com&amp;is_video=false&amp;description=Find Clinical Trials Near You">
-                <img src={htpImg} alt="" height="32" width="32" /></a></li>
-            </ul>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/" className="logo-link">
+              <img src={logoImg} />
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+
+        <Navbar.Collapse>
+          <Nav>
+            {menuItems.map((item, index) =>
+              <li className="nav-item">
+                <Link to={item.link}>{item.text}</Link>
+              </li>
+            )}
+          </Nav>
+
+          <Nav pullRight>
+            <li className="blue-color nav-item">
+              <Link to="/login">Login</Link>
+            </li>
+          </Nav>
+
+          <ul className="social-share nav navbar-nav navbar-right">
+            {socialShares.map((item, index) =>
+              <li key={index}>
+                <a target="_blank" href={item.link}>
+                  <img src={item.image} alt="" height="32" width="32" />
+                </a>
+              </li>
+            )}
+          </ul>
+
+        </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
