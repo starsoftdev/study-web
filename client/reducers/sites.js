2cabb4fedc63176ffd8bef2c0cf4c138433fb054
Thomas Asenov <thomas.asenov85729@gmail.com>
2016-06-28 16:13:27 +0300

    Fix eslint bug

--- client/reducers/sites.js
+++ client/reducers/sites.js
@@ -9,8 +9,8 @@ export default function sites (state=[], action) {
         return action.payload
       }
 
-      return [{id: 1000, name: 'Palmer Tech', principalInvestigator: 'Richard Hendriks', phone: '(111)111-1111', address: '123 Star City', assignedUsers: [{name: 'AssignedUser1'}, {name: 'AssignedUser2'}]},
-        {id: 1001, name: 'Wayne Enterprise', principalInvestigator: 'James Gordon', phone: '(888)888-8888', address: '123 Gotham City', assignedUsers: [{name: 'AssignedUser3'}, {name: 'AssignedUser4'}]}]
+      return [ { id: 1000, name: 'Palmer Tech', principalInvestigator: 'Richard Hendriks', phone: '(111)111-1111', address: '123 Star City', assignedUsers: [ { name: 'AssignedUser1' }, { name: 'AssignedUser2' } ] },
+        { id: 1001, name: 'Wayne Enterprise', principalInvestigator: 'James Gordon', phone: '(888)888-8888', address: '123 Gotham City', assignedUsers: [ { name: 'AssignedUser3' }, { name: 'AssignedUser4' } ] } ]
       //return state
   }
 
