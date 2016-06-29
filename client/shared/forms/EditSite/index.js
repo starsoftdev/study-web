d57f8dbeaa8a6a8c82a880c0acbc2630b900fcce
Thomas Asenov <thomas.asenov85729@gmail.com>
2016-06-29 20:13:13 +0300

    Fix edit site modal default values

--- client/shared/forms/EditSite/index.js
+++ client/shared/forms/EditSite/index.js
@@ -14,8 +14,45 @@ export function getModel (selectedSiteInput) {
   return t.struct(spec)
 }
 
+const nameTemplate = t.form.Form.templates.textbox.clone({
+  renderInput: (locals) => {
+    return <input type="text" className="form-control" name="name" defaultValue={selectedSite.name} onChange={function (evt) { locals.onChange(evt.target.value) }} />
+  }
+})
+
+const principalInvestigatorTemplate = t.form.Form.templates.textbox.clone({
+  renderInput: (locals) => {
+    return <input type="text" className="form-control" name="principalInvestigator" defaultValue={selectedSite.principalInvestigator} onChange={function (evt) { locals.onChange(evt.target.value) }} />
+  }
+})
+
+const phoneTemplate = t.form.Form.templates.textbox.clone({
+  renderInput: (locals) => {
+    return <input type="text" className="form-control" name="phone" defaultValue={selectedSite.phone} onChange={function (evt) { locals.onChange(evt.target.value) }} />
+  }
+})
+
+const addressTemplate = t.form.Form.templates.textbox.clone({
+  renderInput: (locals) => {
+    return <input type="text" className="form-control" name="address" defaultValue={selectedSite.address} onChange={function (evt) { locals.onChange(evt.target.value) }} />
+  }
+})
+
 export const options = {
-  fields: {}
+  fields: {
+    name: {
+      template: nameTemplate
+    },
+    principalInvestigator: {
+      template: principalInvestigatorTemplate
+    },
+    phone: {
+      template: phoneTemplate
+    },
+    address: {
+      template: addressTemplate
+    }
+  }
 }
 
 const EditSiteForm = {
