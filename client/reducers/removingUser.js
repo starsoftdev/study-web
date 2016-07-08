12e6df82f9da470af1a06fc76424b54836ac0761
Thomas Asenov <thomas.asenov85729@gmail.com>
2016-07-09 02:46:37 +0300

    Fix duplicated variable name on removing user reducer

--- client/reducers/removingUser.js
+++ client/reducers/removingUser.js
@@ -1,9 +1,9 @@
 import { ActionTypes } from 'ActionTypes'
 import asyncActionIsFetching from 'utils/asyncActionIsFetching'
 
-const removingUser = asyncActionIsFetching(ActionTypes.DELETE_USER)
+const removingUserStatus = asyncActionIsFetching(ActionTypes.DELETE_USER)
 
 export default function removingUser (state = false, action) {
-  state = removingUser(state, action)
+  state = removingUserStatus(state, action)
   return state
 }
