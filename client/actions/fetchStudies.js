5882683027d5249f17bf996b637ae5973bff13e9
Thomas Asenov <thomas.asenov85729@gmail.com>
2016-08-18 17:09:24 +0300

    Complete filter studylisting component

--- client/actions/fetchStudies.js
+++ client/actions/fetchStudies.js
@@ -1,12 +1,10 @@
-import { bind } from 'redux-effects'
-
 import { ActionTypes } from 'ActionTypes'
-import { searchEntities } from 'utils/entityReadWrite'
+import { createEntity } from 'utils/entityReadWrite'
 import history from 'utils/history'
 import asyncAction from 'utils/asyncAction'
 
 export default function fetchStudies (searchParams) {
-  return asyncAction(ActionTypes.FETCH_STUDIES, (cb, dispatch, getState) => {
-    dispatch(searchEntities('/studies', searchParams, cb))
+  return asyncAction(ActionTypes.FETCH_STUDIES, (cb, dispatch) => {
+    dispatch(createEntity('/studies/search', searchParams, cb))
   })
 }
