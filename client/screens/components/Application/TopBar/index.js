7ed1a53551fcb4627c7f089ce34355511c7c7674
Fabian Bigos <fabianbigos2015@outlook.com>
2016-08-22 05:40:24 +0200

    make dropdown menu disappear on outside click

--- client/screens/components/Application/TopBar/index.js
+++ client/screens/components/Application/TopBar/index.js
@@ -7,6 +7,7 @@ import { logout } from 'actions'
 import isSessionExpired from 'utils/isSessionExpired'
 import history from 'utils/history'
 
+import AvatarMenu from './AvatarMenu'
 import NotificationBox from './NotificationBox'
 
 import './styles.less'
@@ -15,17 +16,12 @@ import studykikLogo from 'assets/images/new_version/logo.svg'
 import avatar1 from 'assets/images/new_version/img2.png'
 import avatar2 from 'assets/images/new_version/img3.png'
 import avatar3 from 'assets/images/new_version/img4.png'
-import userAvatar from 'assets/images/new_version/img-logged-user.png'
 
 export default class TopBar extends React.Component {
   static propTypes = {
     authorization: PropTypes.any,
     location: PropTypes.any,
     logoutRequest: PropTypes.func.isRequired,
-  }
-
-  state = {
-    avatarMenuOpen: false
   }
 
   componentDidMount () {
@@ -46,15 +42,8 @@ export default class TopBar extends React.Component {
     }
   }
 
-  toggleAvatarMenuHandle () {
-    this.setState ({
-      avatarMenuOpen: !this.state.avatarMenuOpen
-    })
-  }
-
   render () {
     const { authorized } = this.props.authorization
-    const avatarMenuClassName = this.state.avatarMenuOpen ? 'avatar-menu-open' : 'avatar-menu-close'
 
     return (
       <header id="header">
@@ -123,28 +112,7 @@ export default class TopBar extends React.Component {
             <a href="#" className="btn btn-default">+ ADD CREDITS</a>
           </div>
 
-          <div className="logged-user-area pull-right">
-            <a className="opener" role="button" onClick={this.toggleAvatarMenuHandle.bind(this)}>
-              <div className="img-circle">
-                <img src={userAvatar} width="43" height="43" alt="Bruce Wayne"  />
-              </div>
-              <span className="text">Bruce Wayne</span>
-              <i className="caret" />
-            </a>
-            <div className={`avatar-menu ${avatarMenuClassName}`}>
-              <div className="well">
-                <ul className="list-unstyled">
-                  <li><a href="#">Profile</a></li>
-                  <li><a href="#">Payment Information</a></li>
-                  <li><a href="#">Reciepts</a></li>
-                  <Link to="/logout"
-                    onClick={this.handleLogoutClick.bind(this)}
-                  >Logout</Link>
-                </ul>
-              </div>
-            </div>
-          </div>
-
+          <AvatarMenu handleLogoutClick={this.handleLogoutClick.bind(this)} />
         </div>
       </header>
     )
