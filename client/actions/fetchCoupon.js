a60c3391d60983d25ac410ef35be02c34f395675
Thomas Asenov <thomas.asenov85729@gmail.com>
2016-09-02 16:56:01 +0300

    Fix coupon url

--- client/actions/fetchCoupon.js
+++ client/actions/fetchCoupon.js
@@ -4,7 +4,6 @@ import asyncAction from 'utils/asyncAction'
 
 export default function fetchCoupon (id) {
   return asyncAction(ActionTypes.FETCH_COUPON, (cb, dispatch) => {
-    const searchParams = { couponId: id }
-    dispatch(searchEntities('/users/retrieveCoupon', searchParams, cb))
+    dispatch(searchEntities('/users/retrieveCoupon/' + id, {}, cb))
   })
 }
