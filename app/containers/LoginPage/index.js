74a5fa8bbc250e672c265f9aa4bc309d6b82a84d
paothegit <pao.tech1018@gmail.com>
2016-09-12 17:47:06 +0300

    auth module implementation

---
+++ app/containers/LoginPage/index.js
@@ -0,0 +1,52 @@
+/**
+ * Login Page
+ */
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
+
+import LoginForm from 'components/LoginForm';
+import { loginRequest } from './actions';
+
+import './styles.less';
+
+class LoginPage extends Component {
+
+  static propTypes = {
+    onSubmitForm: React.PropTypes.func,
+  };
+
+  constructor(props) {
+    super(props);
+    this.onSubmitForm = this.props.onSubmitForm.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="login-page-wrapper">
+        <Helmet title="Login - StudyKIK" />
+        <div className="row">
+          <div className="col-md-4 col-md-push-4">
+            <div className="login-block">
+              <div className="text-center">
+                <h3>StudyKIK Login</h3>
+              </div>
+              <br />
+
+              <LoginForm onSubmit={this.onSubmitForm} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onSubmitForm: (values) => dispatch(loginRequest(values)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(LoginPage);
