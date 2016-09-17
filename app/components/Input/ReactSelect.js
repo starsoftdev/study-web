30216d238554771e2deef2a84a237f434365c4d2
paothegit <pao.tech1018@gmail.com>
2016-09-17 17:55:30 +0300

    react-select, toggle for redux-form inputs

---
+++ app/components/Input/ReactSelect.js
@@ -0,0 +1,76 @@
+/**
+*
+* Input for react-select
+*
+*/
+
+import React, { PropTypes } from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import Select from 'react-select';
+import classNames from 'classnames';
+
+import 'react-select/dist/react-select.min.css';
+import './styles.less';
+
+function ReactSelect({
+  input,
+  name,
+  placeholder,
+  options,
+  className,
+  meta: { touched, error, active },
+}) {
+  const hasError = touched && error && !active;
+  const errorClass = hasError ? 'has-error' : '';
+  const optionsToRender = options.map(o => (
+    { value: (o.value || o.id), label: (o.label || o.name) }
+  ));
+
+  const tooltip = (
+    <Tooltip
+      id={`${name}-tooltip`}
+      className="tooltip-error"
+    >
+      {error}
+    </Tooltip>
+  );
+  let inputComponent = (
+    <Select
+      value={input.value}
+      onChange={input.onChange}
+      onBlur={() => input.onBlur(input.value)}
+      options={optionsToRender}
+      placeholder={placeholder}
+      className="form-control"
+      simpleValue
+    />
+  );
+
+  if (hasError) {
+    inputComponent = (
+      <OverlayTrigger
+        placement="right"
+        overlay={tooltip}
+      >
+        {inputComponent}
+      </OverlayTrigger>
+    );
+  }
+
+  return (
+    <div className={classNames([className, errorClass].join(' '))}>
+      {inputComponent}
+    </div>
+  );
+}
+
+ReactSelect.propTypes = {
+  input: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  options: PropTypes.array,
+  meta: PropTypes.object.isRequired,
+  className: PropTypes.string,
+};
+
+export default ReactSelect;
