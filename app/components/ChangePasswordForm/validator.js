196185ecf80efaccf11ab63a7a105dfd4f833a52
paothegit <pao.tech1018@gmail.com>
2016-09-20 21:58:47 +0300

    some modification to profile page

--- app/components/ChangePasswordForm/validator.js
+++ app/components/ChangePasswordForm/validator.js
@@ -3,7 +3,13 @@ import { validatorFactory } from 'utils/reduxForm';
 const schema = {
   old_password: { presence: true },
   new_password: { presence: true },
-  new_password_confirm: { presence: true },
+  new_password_confirm: {
+    presence: true,
+    equality: {
+      attribute: 'new_password',
+      message: '^Passwords don\'t match!',
+    },
+  },
   user_id: { presence: true },
 };
 
