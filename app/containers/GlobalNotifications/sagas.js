9827871270652e24583845ab0930f1e51c25cc21
Dmitry Nessonov <dmitry@suimm.org>
2016-09-29 13:03:02 +0300

    fix validation errors(lint)

--- app/containers/GlobalNotifications/sagas.js
+++ app/containers/GlobalNotifications/sagas.js
@@ -36,90 +36,86 @@ export function* GlobalNotificationsSaga() {
 
 export function* setSocketConnection() {
   while (true) {
-    const { payload } = yield take(SET_SOCKET_CONNECTION)
-    props = (payload) ? payload.props : null
+    const { payload } = yield take(SET_SOCKET_CONNECTION);
+    props = (payload) ? payload.props : null;
     try {
       if (!socket) {
-        const requestURL = `${SOCKET_URL}/${payload.nsp}`
-        const nsp = io(requestURL)
-        socket = nsp
+        const requestURL = `${SOCKET_URL}/${payload.nsp}`;
+        const nsp = io(requestURL);
+        socket = nsp;
         // const socket = yield call(connect.bind(this, payload))
-        yield put(connectionEstablished(nsp))
-        yield put(toastrActions.success('', 'Connected to socket.'))
-        payload.cb(null, socket)
+        yield put(connectionEstablished(nsp));
+        yield put(toastrActions.success('', 'Connected to socket.'));
+        payload.cb(null, socket);
       }
     } catch (err) {
-      const errorMessage = get(err, 'message', 'Something went wrong!')
-      yield put(toastrActions.error('', errorMessage))
-      payload.cb(err, null)
+      const errorMessage = get(err, 'message', 'Something went wrong!');
+      yield put(toastrActions.error('', errorMessage));
+      payload.cb(err, null);
     }
   }
 }
 
 export function* subscribeToPageEvent() {
-
   while (true) {
-    const { payload } = yield take(SUBSCRIBE_TO_PAGE_EVENT)
+    const { payload } = yield take(SUBSCRIBE_TO_PAGE_EVENT);
     try {
       // console.log('subscribeToPageEvent', payload)
       socket.emit('subscribeToPageEvent', {
-          user: props.currentUser, events: payload.events, params: payload.raw
-        }, (err, data) => {
-          payload.cb(err, data)
-        }
-      )
+        user: props.currentUser, events: payload.events, params: payload.raw,
+      }, (err, data) => {
+        payload.cb(err, data);
+      }
+      );
     } catch (err) {
-      const errorMessage = get(err, 'message', 'Something went wrong!')
-      yield put(toastrActions.error('', errorMessage))
+      const errorMessage = get(err, 'message', 'Something went wrong!');
+      yield put(toastrActions.error('', errorMessage));
     }
   }
 }
 
 export function* subscribeToChatEvent() {
-
   while (true) {
-    const { payload } = yield take(SUBSCRIBE_TO_CHAT_EVENT)
+    const { payload } = yield take(SUBSCRIBE_TO_CHAT_EVENT);
     try {
       socket.emit('subscribeToChatEvent', {
-          user: props.currentUser, events: payload.events, params: payload.raw
-        }, (err, data) => {
-        payload.cb(err, data)
-      })
+        user: props.currentUser, events: payload.events, params: payload.raw,
+      }, (err, data) => {
+        payload.cb(err, data);
+      });
     } catch (err) {
-      const errorMessage = get(err, 'message', 'Something went wrong!')
-      yield put(toastrActions.error('', errorMessage))
+      const errorMessage = get(err, 'message', 'Something went wrong!');
+      yield put(toastrActions.error('', errorMessage));
     }
   }
 }
 
 export function* unsubscribeFromPageEvent() {
-
   while (true) {
-    const { payload } = yield take(UNSUBSCRIBE_FROM_PAGE_EVENT)
+    const { payload } = yield take(UNSUBSCRIBE_FROM_PAGE_EVENT);
     try {
       // console.log('unsubscribeFromPageEvent', payload)
       socket.emit('unsubscribeCurrent', { events: payload.events, params: payload.raw }, (err, data) => {
-        payload.cb(err, data)
-      })
+        payload.cb(err, data);
+      });
     } catch (err) {
-      const errorMessage = get(err, 'message', 'Something went wrong!')
-      yield put(toastrActions.error('', errorMessage))
+      const errorMessage = get(err, 'message', 'Something went wrong!');
+      yield put(toastrActions.error('', errorMessage));
     }
   }
 }
 
 export function* unsubscribeFromAllEvents() {
-
   while (true) {
-    const { payload } = yield take(UNSUBSCRIBE_FROM_ALL)
+    const { payload } = yield take(UNSUBSCRIBE_FROM_ALL);
     try {
       // console.log('unsubscribeFromAllEvents', payload)
       socket.emit('unsubscribeFromAll', { events: payload.events }, (err, data) => {
-        payload.cb(err, data)
-      })
+        payload.cb(err, data);
+      });
     } catch (err) {
-      const errorMessage = get(err, 'message', 'Something went wrong!')
-      yield put(toastrActions.error('', errorMessage))
+      const errorMessage = get(err, 'message', 'Something went wrong!');
+      yield put(toastrActions.error('', errorMessage));
     }
   }
 }
@@ -127,4 +123,4 @@ export function* unsubscribeFromAllEvents() {
 // All sagas to be loaded
 export default [
   GlobalNotificationsSaga,
-]
+];
