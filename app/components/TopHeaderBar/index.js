f275fa253f77c18e9a65887fc404f1182acaf398
Fabian Bigos <fabianbigos2015@outlook.com>
2016-10-06 19:02:16 +0200

    implement fetching notification and count

--- app/components/TopHeaderBar/index.js
+++ app/components/TopHeaderBar/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { createStructuredSelector } from 'reselect';
 
 import studykikLogo from 'assets/images/logo.svg';
 import userAvatar from 'assets/images/img-logged-user.png';
@@ -8,12 +9,21 @@ import AddCreditsModal from 'components/AddCreditsModal';
 
 import NotificationBox from './NotificationBox';
 
+import {
+  selectCurrentUser,
+} from 'containers/App/selectors';
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser(),
+});
+
 const mapDispatchToProps = {
 };
 
-@connect(null, mapDispatchToProps)
+@connect(mapStateToProps, mapDispatchToProps)
 class TopHeaderBar extends React.Component { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
+    currentUser: PropTypes.any,
   }
 
   constructor(props) {
@@ -24,6 +34,15 @@ class TopHeaderBar extends React.Component { // eslint-disable-line react/prefer
     this.state = {
       showAddCreditsModal: false,
     };
+  }
+
+  componentDidMount() {
+    const { currentUser } = this.props;
+
+    if (!currentUser) {
+      console.error('Something is wrong with session');
+      return;
+    }
   }
 
   showAddCreditsModal() {
@@ -41,7 +60,7 @@ class TopHeaderBar extends React.Component { // eslint-disable-line react/prefer
 
           <h1 className="logo pull-left"><a href="#"><img src={studykikLogo} width="214" height="31" alt="logo" /></a></h1>
 
-          <NotificationBox />
+          <NotificationBox currentUser={this.props.currentUser} />
 
           <div className="emails pull-left">
             <a href="#" className="opener" data-toggle="tooltip" data-placement="bottom" title="Coming Soon">
--- app/containers/GlobalNotifications/constants.js
+++ app/containers/GlobalNotifications/constants.js
@@ -13,3 +13,8 @@ export const UNSUBSCRIBE_FROM_PAGE_EVENT = 'app/GlobalNotifications/UNSUBSCRIBE_
 export const UNSUBSCRIBE_FROM_ALL = 'app/GlobalNotifications/UNSUBSCRIBE_FROM_ALL';
 export const SUBSCRIBE_TO_CHAT_EVENT = 'app/GlobalNotifications/SUBSCRIBE_TO_CHAT_EVENT';
 
+export const FETCH_NOTIFICATIONS = 'app/GlobalNotifications/FETCH_NOTIFICATIONS';
+export const FETCH_NOTIFICATIONS_SUCCESS = 'app/GlobalNotifications/FETCH_NOTIFICATIONS_SUCCESS';
+export const FETCH_UNREAD_NOTIFICATIONS_COUNT = 'app/GlobalNotifications/FETCH_UNREAD_NOTIFICATIONS_COUNT';
+export const FETCH_UNREAD_NOTIFICATIONS_COUNT_SUCCESS = 'app/GlobalNotifications/FETCH_UNREAD_NOTIFICATIONS_COUNT_SUCCESS';
+export const RECEIVE_NOTIFICATION = 'app/GlobalNotifications/RECEIVE_NOTIFICATION';
