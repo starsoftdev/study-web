20824f92436972c68ac562ca67f181ba0ae9dc55
Thomas Asenov <thomas.asenov85729@gmail.com>
2016-10-14 22:25:07 +0300

    Move sites and users data to global scope

--- app/common/sagas/baseData.saga.js
+++ app/common/sagas/baseData.saga.js
@@ -1,11 +1,11 @@
-/*
- *  saga to load auth user details from the just token
- */
+/* eslint-disable no-constant-condition, consistent-return */
+
 import { take, call, put, fork } from 'redux-saga/effects';
 import { actions as toastrActions } from 'react-redux-toastr';
 import { get } from 'lodash';
 
 import request from 'utils/request';
+import composeQueryString from 'utils/composeQueryString';
 import {
   FETCH_SITES,
   FETCH_INDICATIONS,
@@ -16,7 +16,17 @@ import {
   SAVE_CARD,
   DELETE_CARD,
   ADD_CREDITS,
+
+  FETCH_CLIENT_SITES,
+  FETCH_CLIENT_ROLES,
+  FETCH_SITE,
+  FETCH_USER,
+  DELETE_USER,
+  DELETE_CLIENT_ROLE,
+  SAVE_SITE,
+  SAVE_USER,
 } from 'containers/App/constants';
+
 
 import {
   sitesFetched,
@@ -37,6 +47,23 @@ import {
   cardDeletingError,
   creditsAdded,
   creditsAddingError,
+
+  clientSitesFetched,
+  clientSitesFetchingError,
+  clientRolesFetched,
+  clientRolesFetchingError,
+  siteFetched,
+  siteFetchingError,
+  userFetched,
+  userFetchingError,
+  userDeleted,
+  userDeletingError,
+  clientRoleDeleted,
+  clientRoleDeletingError,
+  siteSaved,
+  siteSavingError,
+  userSaved,
+  userSavingError,
 } from 'containers/App/actions';
 
 export default function* baseDataSaga() {
@@ -49,6 +76,15 @@ export default function* baseDataSaga() {
   yield fork(saveCardWatcher);
   yield fork(deleteCardWatcher);
   yield fork(addCreditsWatcher);
+
+  yield fork(fetchClientSitesWatcher);
+  yield fork(fetchClientRolesWatcher);
+  yield fork(fetchSiteWatcher);
+  yield fork(fetchUserWatcher);
+  yield fork(deleteUserWatcher);
+  yield fork(deleteClientRoleWatcher);
+  yield fork(saveSiteWatcher);
+  yield fork(saveUserWatcher);
 }
 
 export function* fetchSitesWatcher() {
@@ -194,8 +230,11 @@ export function* deleteCardWatcher() {
       const requestURL = `${API_URL}/clients/stripe_customer/${customerId}/delete_card/${cardId}`;
       const response = yield call(request, requestURL, options);
 
+      yield put(toastrActions.success('Delete Card', 'Card deleted successfully!'));
       yield put(cardDeleted(response));
     } catch (err) {
+      const errorMessage = get(err, 'message', 'Something went wrong while submitting your request');
+      yield put(toastrActions.error('', errorMessage));
       yield put(cardDeletingError(err));
     }
   }
@@ -219,6 +258,216 @@ export function* addCreditsWatcher() {
       const errorMessage = get(err, 'message', 'Something went wrong while submitting your request');
       yield put(toastrActions.error('', errorMessage));
       yield put(creditsAddingError(err));
+    }
+  }
+}
+
+export function* fetchClientSitesWatcher() {
+  while (true) {
+    const { clientId, searchParams } = yield take(FETCH_CLIENT_SITES);
+
+    try {
+      const filterObj = {
+        include: ['users', 'studies'],
+        where: {},
+      };
+
+      if (searchParams && searchParams.name) {
+        filterObj.where = {
+          name: {
+            like: `%${searchParams.name}%`,
+            options: 'i',
+          },
+        };
+      }
+
+      const queryParams = {
+        filter: JSON.stringify(filterObj),
+      };
+      const queryString = composeQueryString(queryParams);
+      const requestURL = `${API_URL}/clients/${clientId}/sites?${queryString}`;
+      const response = yield call(request, requestURL);
+
+      yield put(clientSitesFetched(response));
+    } catch (err) {
+      yield put(clientSitesFetchingError(err));
+    }
+  }
+}
+
+export function* fetchClientRolesWatcher() {
+  while (true) {
+    const { clientId, searchParams } = yield take(FETCH_CLIENT_ROLES);
+
+    try {
+      const filterObj = {
+        include: 'user',
+        where: {},
+      };
+
+      if (searchParams && searchParams.name) {
+        filterObj.where = {
+          name: {
+            like: `%${searchParams.name}%`,
+            options: 'i',
+          },
+        };
+      }
+
+      const queryParams = {
+        filter: JSON.stringify(filterObj),
+      };
+      const queryString = composeQueryString(queryParams);
+      const requestURL = `${API_URL}/clients/${clientId}/roles?${queryString}`;
+      const response = yield call(request, requestURL);
+
+      yield put(clientRolesFetched(response));
+    } catch (err) {
+      yield put(clientRolesFetchingError(err));
+    }
+  }
+}
+
+export function* fetchSiteWatcher() {
+  while (true) {
+    const { id } = yield take(FETCH_SITE);
+
+    try {
+      const requestURL = `${API_URL}/sites/${id}`;
+      const response = yield call(request, requestURL);
+
+      yield put(siteFetched(response));
+    } catch (err) {
+      yield put(siteFetchingError(err));
+    }
+  }
+}
+
+export function* fetchUserWatcher() {
+  while (true) {
+    const { id } = yield take(FETCH_USER);
+
+    try {
+      const queryParams = { filter: '{"include":"roleForClient"}' };
+      const queryString = composeQueryString(queryParams);
+      const requestURL = `${API_URL}/users/${id}?${queryString}`;
+      const response = yield call(request, requestURL);
+
+      yield put(userFetched(response));
+    } catch (err) {
+      yield put(userFetchingError(err));
+    }
+  }
+}
+
+export function* deleteUserWatcher() {
+  while (true) {
+    const { id } = yield take(DELETE_USER);
+
+    try {
+      const requestURL = `${API_URL}/users/${id}`;
+      const options = {
+        method: 'DELETE',
+      };
+      const response = yield call(request, requestURL, options);
+
+      yield put(toastrActions.success('Delete User', 'User deleted successfully!'));
+      yield put(userDeleted(id, response));
+    } catch (err) {
+      const errorMessage = get(err, 'message', 'Something went wrong while submitting your request');
+      yield put(toastrActions.error('', errorMessage));
+      yield put(userDeletingError(err));
+    }
+  }
+}
+
+export function* deleteClientRoleWatcher() {
+  while (true) {
+    const { id } = yield take(DELETE_CLIENT_ROLE);
+
+    try {
+      const requestURL = `${API_URL}/clientRoles/${id}`;
+      const options = {
+        method: 'DELETE',
+      };
+      const response = yield call(request, requestURL, options);
+
+      yield put(toastrActions.success('Delete Client Role', 'Client Role deleted successfully!'));
+      yield put(clientRoleDeleted(id, response));
+    } catch (err) {
+      const errorMessage = get(err, 'message', 'Something went wrong while submitting your request');
+      yield put(toastrActions.error('', errorMessage));
+      yield put(clientRoleDeletingError(err));
+    }
+  }
+}
+
+export function* saveSiteWatcher() {
+  while (true) {
+    const { clientId, id, data } = yield take(SAVE_SITE);
+
+    try {
+      let requestURL = null;
+      let options = null;
+      data.client_id = clientId;
+
+      if (id) {
+        requestURL = `${API_URL}/sites/${id}`;
+        options = {
+          method: 'PUT',
+          body: JSON.stringify(data),
+        };
+      } else {
+        requestURL = `${API_URL}/sites`;
+        options = {
+          method: 'POST',
+          body: JSON.stringify(data),
+        };
+      }
+
+      const response = yield call(request, requestURL, options);
+
+      yield put(toastrActions.success('Save Site', 'Site saved successfully!'));
+      yield put(siteSaved(response));
+    } catch (err) {
+      const errorMessage = get(err, 'message', 'Something went wrong while submitting your request');
+      yield put(toastrActions.error('', errorMessage));
+      yield put(siteSavingError(err));
+    }
+  }
+}
+
+export function* saveUserWatcher() {
+  while (true) {
+    const { clientId, id, data } = yield take(SAVE_USER);
+
+    try {
+      let requestURL = null;
+      let options = null;
+
+      if (id) {
+        data.userId = id;
+        requestURL = `${API_URL}/clients/${clientId}/updateUserWithClientRole`;
+        options = {
+          method: 'PUT',
+          body: JSON.stringify(data),
+        };
+      } else {
+        requestURL = `${API_URL}/clients/${clientId}/addUserWithClientRole`;
+        options = {
+          method: 'POST',
+          body: JSON.stringify(data),
+        };
+      }
+
+      const response = yield call(request, requestURL, options);
+
+      yield put(toastrActions.success('Save User', 'User saved successfully!'));
+      yield put(userSaved(data.siteId, response));
+    } catch (err) {
+      const errorMessage = get(err, 'message', 'Something went wrong while submitting your request');
+      yield put(toastrActions.error('', errorMessage));
+      yield put(userSavingError(err));
     }
   }
 }
