88bb47f90585abb2e6b7dbb5c0fd4324a500364a
Mike Li <mike@instataskapp.com>
2016-10-19 21:34:06 -0700

    Receive errors for the form so we can handle it properly.

--- app/containers/StudyPage/PatientDetail/validator.js
+++ app/containers/StudyPage/PatientDetail/validator.js
@@ -8,6 +8,7 @@ const schema = {
     presence: true,
   },
   phone: {
+    presence: true,
     format: {
       // must be a phone in the format of (123) 456-7890 or E.164 format phone numbers
       pattern: '^\\(\\d{3}\\)\\s?\\d{3}\\-\\d{4}|\\+?[1-9]\\d{1,14}$',
