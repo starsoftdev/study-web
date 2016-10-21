30d217c533876c056f58c8e0579c7894af39d2e9
Mike Li <mike@instataskapp.com>
2016-10-21 03:09:51 -0700

    Adds preliminary support for drag and drop.

--- app/containers/StudyPage/PatientPanel/Patient.js
+++ app/containers/StudyPage/PatientPanel/Patient.js
@@ -5,16 +5,18 @@
 import React from 'react';
 import classNames from 'classnames';
 import moment from 'moment-timezone';
+import { DragSource } from 'react-dnd';
 import { formatPhone } from '../helper/functions';
+import DragTypes from './dragSourceTypes';
 
 class Patient extends React.Component {
 
   static propTypes = {
     category: React.PropTypes.object.isRequired,
-    // connectDragSource: React.PropTypes.func.isRequired,
+    connectDragSource: React.PropTypes.func.isRequired,
     currentUser: React.PropTypes.object.isRequired,
     currentPatientId: React.PropTypes.number,
-    // isDragging: React.PropTypes.bool.isRequired,
+    isDragging: React.PropTypes.bool.isRequired,
     onPatientClick: React.PropTypes.func.isRequired,
     patient: React.PropTypes.object.isRequired,
   };
@@ -45,9 +47,9 @@ class Patient extends React.Component {
   }
 
   render() {
-    const { isDragging, connectDragSource, category, currentPatientId, onPatientClick, patient } = this.props;
+    const { connectDragSource, category, currentPatientId, onPatientClick, patient } = this.props;
     const patientPhone = formatPhone(patient.phone);
-    return (
+    return connectDragSource(
       <li
         className={classNames({ 'patient-selected': patient.id === currentPatientId })}
         onClick={() => {
@@ -69,4 +71,30 @@ class Patient extends React.Component {
   }
 }
 
-export default Patient;
+/**
+ * Specifies the drag source contract.
+ * Only `beginDrag` function is required.
+ */
+const patientSource = {
+  isDragging(props, monitor) {
+    // If your component gets unmounted while dragged
+    // you can implement something like this to keep its
+    // appearance dragged:
+    return monitor.getItem().id === props.patient.id;
+  },
+  beginDrag(props, monitor, component) {
+    // Return the data describing the dragged item
+    const item = { id: props.patient.id };
+    return item;
+  },
+};
+
+const collect = (connect, monitor) => ({
+  // Call this function inside render()
+  // to let React DnD handle the drag events:
+  connectDragSource: connect.dragSource(),
+  // You can ask the monitor about the current drag state:
+  isDragging: monitor.isDragging(),
+});
+
+export default DragSource(DragTypes.PATIENT, patientSource, collect)(Patient);
---
+++ app/containers/StudyPage/PatientPanel/dragSourceTypes.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mike on 10/21/16.
+ */
+
+// Drag sources and drop targets only interact
+// if they have the same string type.
+export default {
+  PATIENT: 'patient',
+}
