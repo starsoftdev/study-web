d2203b8b70f10597532ae0a06f6affbb5260b70b
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2016-10-21 13:17:41 +0300

    KIK-746. Add validation.

--- app/components/ManageTransferNumberForm/validator.js
+++ app/components/ManageTransferNumberForm/validator.js
@@ -1,6 +1,23 @@
-import { validatorFactory } from 'utils/reduxForm';
+const validate = values => {
+  const errors = {};
+  errors.sourcesList = [];
+  if (values.sourcesList) {
+    values.sourcesList.forEach((source, index) => {
+      if (source.twilioRedirectNumber && source.twilioRedirectNumber.phoneNumber) {
+        const regex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
 
-const schema = {
+        if (!regex.test(source.twilioRedirectNumber.phoneNumber)) {
+          errors.sourcesList[index] = {};
+          errors.sourcesList[index].twilioRedirectNumber = {};
+          errors.sourcesList[index].twilioRedirectNumber.phoneNumber = {};
+          errors.sourcesList[index].twilioRedirectNumber.phoneNumber._error = 'No more than five hobbies allowed'; // eslint-disable-line no-underscore-dangle
+          // Valid international phone number
+        }
+      }
+    });
+  }
+
+  return errors;
 };
 
-export default validatorFactory(schema);
+export default validate;
