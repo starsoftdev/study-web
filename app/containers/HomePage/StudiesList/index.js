be883e46893bf65c73d81ae6685a5084c10a1334
Thomas Asenov <thomas.asenov85729@gmail.com>
2016-10-23 12:35:25 +0300

    Fix some styles and variable constants

--- app/containers/HomePage/StudiesList/index.js
+++ app/containers/HomePage/StudiesList/index.js
@@ -1,8 +1,10 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { countBy } from 'lodash';
 
 import { selectStudies } from 'containers/HomePage/selectors';
+import { ACTIVE_STATUS_VALUE, INACTIVE_STATUS_VALUE } from 'containers/HomePage/constants';
 import StudyItem from './StudyItem';
 import './styles.less';
 
@@ -13,55 +15,58 @@ class StudiesList extends Component { // eslint-disable-line react/prefer-statel
 
   render() {
     const { studies } = this.props;
-    const activeCount = 13;
-    const inactiveCount = 11;
-    const totalCount = 24;
+    const countResult = countBy(studies.details, entityIterator => entityIterator.status);
+    const activeCount = countResult[ACTIVE_STATUS_VALUE] || 0;
+    const inactiveCount = countResult[INACTIVE_STATUS_VALUE] || 0;
+    const totalCount = studies.details.length;
     const studiesListContents = studies.details.map((item, index) => (
       <StudyItem {...item} key={index} index={index} />
     ));
 
     if (studies.details.length > 0) {
       return (
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="table-responsive">
-              <table className="table table-striped">
-                <caption>
-                  <span className="pull-left">Study/Site Status</span>
-                  <span className="pull-right">
-                    <span className="inner-info">
-                      <span className="info-label">ACTIVE</span>
-                      <span className="info-value">{activeCount}</span>
+        <div className="studies">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="table-responsive">
+                <table className="table table-striped">
+                  <caption>
+                    <span className="pull-left">Study/Site Status</span>
+                    <span className="pull-right">
+                      <span className="inner-info">
+                        <span className="info-label">ACTIVE</span>
+                        <span className="info-value">{activeCount}</span>
+                      </span>
+                      <span className="inner-info">
+                        <span className="info-label">INACTIVE</span>
+                        <span className="info-value">{inactiveCount}</span>
+                      </span>
+                      <span className="inner-info">
+                        <span className="info-label">TOTAL</span>
+                        <span className="info-value">{totalCount}</span>
+                      </span>
                     </span>
-                    <span className="inner-info">
-                      <span className="info-label">INACTIVE</span>
-                      <span className="info-value">{inactiveCount}</span>
-                    </span>
-                    <span className="inner-info">
-                      <span className="info-label">TOTAL</span>
-                      <span className="info-value">{totalCount}</span>
-                    </span>
-                  </span>
-                </caption>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>INDICATION</th>
-                    <th>LOCATION</th>
-                    <th>SPONSOR</th>
-                    <th>PROTOCOL</th>
-                    <th>
-                      <span className="icon-credit" data-original-title="Patient Messaging Suite"></span>
-                    </th>
-                    <th>STATUS</th>
-                    <th>START DATE</th>
-                    <th>END DATE</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {studiesListContents}
-                </tbody>
-              </table>
+                  </caption>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>INDICATION</th>
+                      <th>LOCATION</th>
+                      <th>SPONSOR</th>
+                      <th>PROTOCOL</th>
+                      <th>
+                        <span className="icon-credit" data-original-title="Patient Messaging Suite"></span>
+                      </th>
+                      <th>STATUS</th>
+                      <th>START DATE</th>
+                      <th>END DATE</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {studiesListContents}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
