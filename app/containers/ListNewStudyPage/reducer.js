dc6f892e36fb38462994d30e6ae48e5dc8c238d5
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2016-10-25 16:54:21 +0300

    KIK-639. Add modal to show after list new study form submission, add IrbAd creation stripe product.

--- app/containers/ListNewStudyPage/reducer.js
+++ app/containers/ListNewStudyPage/reducer.js
@@ -10,6 +10,10 @@ import {
   SHOW_ADD_EMAIL_MODAL,
   HIDE_ADD_EMAIL_MODAL,
   GET_AVAIL_PHONE_NUMBERS_SUCCESS,
+  SUBMIT_FORM,
+  SUBMIT_FORM_SUCCESS,
+  SUBMIT_FORM_ERROR,
+  HIDE_SUBMIT_FORM_MODAL,
 } from './constants';
 
 import _ from 'lodash';
@@ -17,7 +21,13 @@ import _ from 'lodash';
 const initialState = {
   showAddSiteLocationModal: false,
   showAddEmailModal: false,
+  showSubmitFormModal: false,
   availPhoneNumbers: [],
+  formSubmissionStatus: {
+    submitting: false,
+    error: null,
+    response: null,
+  },
 };
 
 function listNewStudyPageReducer(state = initialState, action) {
@@ -46,6 +56,39 @@ function listNewStudyPageReducer(state = initialState, action) {
       return {
         ...state,
         availPhoneNumbers: _.map(action.payload.avail, (value) => ({ ...value, value:value.phoneNumber, label: value.friendlyName })),
+      };
+    case SUBMIT_FORM:
+      return {
+        ...state,
+        showSubmitFormModal: true,
+        formSubmissionStatus: {
+          submitting: true,
+          error: null,
+          response: null,
+        },
+      };
+    case SUBMIT_FORM_SUCCESS:
+      return {
+        ...state,
+        formSubmissionStatus: {
+          submitting: false,
+          error: null,
+          response: action.payload,
+        },
+      };
+    case SUBMIT_FORM_ERROR:
+      return {
+        ...state,
+        formSubmissionStatus: {
+          submitting: false,
+          error: action.payload,
+          response: null,
+        },
+      };
+    case HIDE_SUBMIT_FORM_MODAL:
+      return {
+        ...state,
+        showSubmitFormModal: false,
       };
     default:
       return state;
