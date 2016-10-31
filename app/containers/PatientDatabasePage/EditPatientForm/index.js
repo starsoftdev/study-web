170964f8ba141b6973bdc63c45ad4c10c6f10709
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2016-10-31 14:21:26 +0200

    KIK-854. Fix problem with react multi select component.

--- app/containers/PatientDatabasePage/EditPatientForm/index.js
+++ app/containers/PatientDatabasePage/EditPatientForm/index.js
@@ -125,6 +125,7 @@ class EditPatientForm extends Component { // eslint-disable-line react/prefer-st
                 options={indicationOptions}
                 multi
                 joinValues
+                objectValue={true}
                 disabled={savedPatient.saving}
               />
             </div>
