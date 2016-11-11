e9e612055a9fe4320e729ab40bad15b2c26a04bf
Fabian Bigos <fabianbigos2015@outlook.com>
2016-11-11 16:11:32 +0100

    Fix eslint error in calendagePage

--- app/containers/CalendarPage/sagas.js
+++ app/containers/CalendarPage/sagas.js
@@ -7,7 +7,7 @@ import { LOCATION_CHANGE } from 'react-router-redux';
 import { actions as toastrActions } from 'react-redux-toastr';
 import { get } from 'lodash';
 
-import { getItem } from 'utils/localStorage';
+import { getItem, removeItem } from 'utils/localStorage';
 import request from 'utils/request';
 
 import {
