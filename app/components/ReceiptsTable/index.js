5c875f34af37f0b7aff7652b4be621790fcc5f6b
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2016-11-11 15:49:27 +0200

    KIK-854.  Lazy loading for receipts page.

--- app/components/ReceiptsTable/index.js
+++ app/components/ReceiptsTable/index.js
@@ -14,19 +14,19 @@ import InfiniteScroll from 'react-infinite-scroller';
 const headers = [
   {
     text: 'Date',
-    sort: 'date',
+    sort: 'created',
   },
   {
     text: 'Site name',
-    sort: 'site',
+    sort: 'site_name',
   },
   {
     text: 'Invoice number',
-    sort: 'invoice',
+    sort: 'invoice_id',
   },
   {
     text: 'Protocol number',
-    sort: 'protocol',
+    sort: 'protocol_number',
   },
   {
     text: 'Payment type',
@@ -45,7 +45,9 @@ class ReceiptsTable extends Component { // eslint-disable-line react/prefer-stat
     searchBy: PropTypes.any,
     receipts: PropTypes.array,
     getReceipts: PropTypes.func,
-    hasMoreItems: PropTypes.bool,
+    paginationOptions: PropTypes.object,
+    searchOptions: PropTypes.array,
+    setActiveSort: PropTypes.func,
   };
 
   constructor(props) {
@@ -145,9 +147,10 @@ class ReceiptsTable extends Component { // eslint-disable-line react/prefer-stat
     this.SelectedReceipts = value;
   }
 
-  loadItems(page) {
-    console.log('load items ', page);
-    this.props.getReceipts(15, 0);
+  loadItems() {
+    const limit = 15;
+    const offset = (this.props.paginationOptions.page) * 15;
+    this.props.getReceipts(limit, offset, this.props.receipts, this.props.paginationOptions.activeSort, this.props.paginationOptions.activeDirection, this.props.searchOptions);
   }
 
   sortBy(ev) {
@@ -158,128 +161,37 @@ class ReceiptsTable extends Component { // eslint-disable-line react/prefer-stat
     if (ev.currentTarget.className && ev.currentTarget.className.indexOf('down') !== -1) {
       direction = 'up';
     }
-    console.log(direction);
 
-    const receiptsArr = this.state.filteredReceipts || this.props.receipts;
-    const directionUnits = (direction === 'up') ? {
-      more: 1,
-      less: -1,
-    } : {
-      more: -1,
-      less: 1,
-    };
-
-    switch (sort) {
-      case 'date':
-        receiptsArr.sort((a, b) => {
-          const aDate = new Date(a.created).getTime();
-          const bDate = new Date(b.created).getTime();
-
-          if (aDate > bDate) {
-            return directionUnits.more;
-          }
-          if (aDate < bDate) {
-            return directionUnits.less;
-          }
-          return 0;
-        });
-        break;
-      case 'site':
-        receiptsArr.sort((a, b) => {
-          const siteNameA = (a.invoiceDetails[0].campaign) ? a.invoiceDetails[0].campaign.site.name : a.sites.name;
-          const siteNameB = (b.invoiceDetails[0].campaign) ? b.invoiceDetails[0].campaign.site.name : b.sites.name;
-
-          if (siteNameA > siteNameB) {
-            return directionUnits.more;
-          }
-          if (siteNameA < siteNameB) {
-            return directionUnits.less;
-          }
-          return 0;
-        });
-        break;
-      case 'payment':
-        receiptsArr.sort((a, b) => {
-          if ((a.payment_method_id !== null && b.payment_method_id !== null)
-            && a.paymentMethod.nickname > b.paymentMethod.nickname) {
-            return directionUnits.more;
-          }
-          if ((a.payment_method_id !== null && b.payment_method_id !== null)
-            && a.paymentMethod.nickname < b.paymentMethod.nickname) {
-            return directionUnits.less;
-          }
-          return 0;
-        });
-        break;
-      case 'protocol':
-        receiptsArr.sort((a, b) => {
-          const protocolNumberA = (a.invoiceDetails[0].campaign) ? a.invoiceDetails[0].campaign.study.protocolNumber : '-';
-          const protocolNumberB = (b.invoiceDetails[0].campaign) ? b.invoiceDetails[0].campaign.study.protocolNumber : '-';
-
-          if (protocolNumberA > protocolNumberB) {
-            return directionUnits.more;
-          }
-          if (protocolNumberA < protocolNumberB) {
-            return directionUnits.less;
-          }
-          return 0;
-        });
-        break;
-      case 'total':
-        receiptsArr.sort((a, b) => {
-          if (a.total > b.total) {
-            return directionUnits.more;
-          }
-          if (a.total < b.total) {
-            return directionUnits.less;
-          }
-          return 0;
-        });
-        break;
-      case 'invoice':
-        receiptsArr.sort((a, b) => {
-          if (a.id > b.id) {
-            return directionUnits.more;
-          }
-          if (a.id < b.id) {
-            return directionUnits.less;
-          }
-          return 0;
-        });
-        break;
-      default:
-        break;
+    if (sort !== 'payment') {
+      this.props.setActiveSort(sort, direction);
+      this.props.getReceipts(15, 0, this.props.receipts, sort, direction, this.props.searchOptions);
     }
-
-    this.setState({
-      filteredReceipts: receiptsArr,
-      activeSort: sort,
-      activeDirection: direction,
-    });
   }
 
   mapHeaders(raw, state, result) {
     _.map(raw, (header, key) => {
-
       let width = '';
-      switch(key){
+      switch (key) {
         case 0:
-          width = '9.6%'
+          width = '9.6%';
           break;
         case 1:
-          width = '17.4%'
+          width = '17.4%';
           break;
         case 2:
-          width = '18.4%'
+          width = '18.4%';
           break;
         case 3:
-          width = '20.7%'
+          width = '19.7%';
           break;
         case 4:
-          width = '17.4%'
+          width = '17.4%';
           break;
         case 5:
-          width = '9.5%'
+          width = '9.5%';
+          break;
+        default:
+          width = 'auto';
           break;
       }
 
@@ -288,8 +200,8 @@ class ReceiptsTable extends Component { // eslint-disable-line react/prefer-stat
           key={key}
           data-sort={header.sort}
           onClick={this.sortBy}
-          className={`th ${(state.activeSort === header.sort) ? state.activeDirection : ''}`}
-          style={{ width: width }}
+          className={`th ${(this.props.paginationOptions.activeSort === header.sort) ? this.props.paginationOptions.activeDirection : ''}`}
+          style={{ width }}
         >
           {header.text} <i className="caret-arrow" />
         </div>
@@ -300,98 +212,49 @@ class ReceiptsTable extends Component { // eslint-disable-line react/prefer-stat
   mapReceipts(raw, result) {
     _.map(raw, (source, key) => {
       const date = new Date(source.created);
-      const dateWrapper = moment(date).format('YYYY/MM/DD');
+      const dateWrapper = moment(date).format('MM/DD/YY');
       const sub = ((source.total % 100) === 0) ? '.00' : false;
 
-      if (source.invoiceDetails.length) {
-        let site = '-';
-        const protocol = (source.invoiceDetails[0].campaign) ? source.invoiceDetails[0].campaign.study.protocolNumber : '-';
-        if (source.invoiceDetails[0].campaign) {
-          site = source.invoiceDetails[0].campaign.site.name;
-        } else if (source.sites) {
-          site = source.sites.name;
-        }
-
-        result.push(
-          <tr key={key}>
-            <td>
-              <span className={(source.selected) ? 'sm-container checked' : 'sm-container'}>
-                <span className="input-style" onClick={this.onClickCurrent}>
-                  <input type="checkbox" name={key} />
-                </span>
+      result.push(
+        <div className="tr" key={key}>
+          <div className="td" style={{ width: '8%' }}>
+            <span className={(source.selected) ? 'sm-container checked' : 'sm-container'}>
+              <span className="input-style" onClick={this.onClickCurrent}>
+                <input type="checkbox" name={key} />
               </span>
-            </td>
-            <td>{dateWrapper}</td>
-            <td>{site}</td>
-            <td>{source.invoiceDetails[0].invoice_id}</td>
-            <td>{protocol}</td>
-            <td>card</td>
-            <td>${(sub) ? `${(source.total / 100)}${sub}` : `${(source.total / 100).toFixed(2)}` }</td>
-          </tr>
-        );
-      }
-    });
-  }
-
-  mapReceipts2(raw, result) {
-    _.map(raw, (source, key) => {
-      const date = new Date(source.created);
-      const dateWrapper = moment(date).format('YYYY/MM/DD');
-      const sub = ((source.total % 100) === 0) ? '.00' : false;
-
-      if (source.invoiceDetails.length) {
-        let site = '-';
-        const protocol = (source.invoiceDetails[0].campaign) ? source.invoiceDetails[0].campaign.study.protocolNumber : '-';
-        if (source.invoiceDetails[0].campaign) {
-          site = source.invoiceDetails[0].campaign.site.name;
-        } else if (source.sites) {
-          site = source.sites.name;
-        }
-
-        result.push(
-          <div className="tr" key={key}>
-            <div className="td" style={{ width: '7%' }}>
-              <span className={(source.selected) ? 'sm-container checked' : 'sm-container'}>
-                <span className="input-style" onClick={this.onClickCurrent}>
-                  <input type="checkbox" name={key} />
-                </span>
-              </span>
-            </div>
-            <div className="td" style={{ width: '9.6%' }}>{dateWrapper}</div>
-            <div className="td" style={{ width: '17.4%' }}>{site}</div>
-            <div className="td" style={{ width: '18.4%' }}>{source.invoiceDetails[0].invoice_id}</div>
-            <div className="td" style={{ width: '20.7%' }}>{protocol}</div>
-            <div className="td" style={{ width: '17.4%' }}>card</div>
-            <div className="td" style={{ width: '9.5%' }}>${(sub) ? `${(source.total / 100)}${sub}` : `${(source.total / 100).toFixed(2)}` }</div>
+            </span>
+            <span>{(key + 1)}</span>
           </div>
-        );
-      }
+          <div className="td" style={{ width: '9.6%' }}>{dateWrapper}</div>
+          <div className="td" style={{ width: '17.4%' }}>{source.site_name}</div>
+          <div className="td" style={{ width: '18.4%' }}>{source.invoice_id}</div>
+          <div className="td" style={{ width: '19.7%' }}>{source.protocol_number}</div>
+          <div className="td" style={{ width: '17.4%' }}>card</div>
+          <div className="td" style={{ width: '9.5%' }}>${(sub) ? `${(source.total / 100)}${sub}` : `${(source.total / 100).toFixed(2)}` }</div>
+        </div>
+      );
     });
   }
 
   render() {
-    console.log('render');
     const state = this.state;
-    //const receiptsArr = state.filteredReceipts || this.props.receipts;
     const receipts = [];
-    const receipts2 = [];
     const heads = [];
 
     this.mapHeaders(headers, state, heads);
     this.mapReceipts(this.props.receipts, receipts);
-    this.mapReceipts2(this.props.receipts, receipts2);
 
     return (
       <div className="table-holder">
         <StickyContainer className="table-sticky">
           <Sticky className="header">
             <div className="tr">
-              <div className="th" style={{ width: '7%' }}>
-                  <span className={(this.state.checkAll) ? 'sm-container checked' : 'sm-container'}>
-                    <span className="input-style" onClick={this.onClickAll}>
-                      <input name="all" type="checkbox" />
-                    </span>
+              <div className="th" style={{ width: '8%' }}>
+                <span className={(this.state.checkAll) ? 'sm-container checked' : 'sm-container'}>
+                  <span className="input-style" onClick={this.onClickAll}>
+                    <input name="all" type="checkbox" />
                   </span>
+                </span>
                 <span>#</span><i className="caret-arrow" />
               </div>
               {heads}
@@ -403,30 +266,12 @@ class ReceiptsTable extends Component { // eslint-disable-line react/prefer-stat
             pageStart={0}
             loadMore={this.loadItems}
             initialLoad={false}
-            hasMore={this.props.hasMoreItems}
+            hasMore={this.props.paginationOptions.hasMoreItems}
             loader={<div>Loading...</div>}
           >
-            {receipts2}
+            {receipts}
           </InfiniteScroll>
 
-          {/*<table className="table">
-            <colgroup>
-              <col style={{ width: '7%' }} />
-              <col style={{ width: '9.6%' }} />
-              <col style={{ width: '17.4%' }} />
-              <col style={{ width: '18.4%' }} />
-              <col style={{ width: '20.7%' }} />
-              <col style={{ width: '17.4%' }} />
-              <col style={{ width: '9.5%' }} />
-            </colgroup>
-
-            <tbody>
-            {receipts}
-            </tbody>
-
-            
-          </table>*/}
-          
         </StickyContainer>
       </div>
     );
--- app/containers/Receipts/actions.js
+++ app/containers/Receipts/actions.js
@@ -9,6 +9,8 @@ import {
   GET_RECEIPT,
   RECEIPTS_RECEIVED,
   GET_PDF,
+  SET_SEARCH_OPTIONS,
+  SET_ACTIVE_SORT,
 } from './constants';
 
 export function defaultAction() {
@@ -17,18 +19,23 @@ export function defaultAction() {
   };
 }
 
-export function receiptsReceived(payload) {
+export function receiptsReceived(payload, hasMore, page) {
   return {
     type: RECEIPTS_RECEIVED,
     payload,
+    hasMore,
+    page,
   };
 }
 
-export function getReceipts(limit, offset, payload) {
+export function getReceipts(limit, offset, receipts, orderBy, orderDir, payload) {
   return {
     type: GET_RECEIPT,
     limit,
     offset,
+    receipts,
+    orderBy,
+    orderDir,
     payload,
   };
 }
@@ -37,5 +44,20 @@ export function getPDF(payload) {
   return {
     type: GET_PDF,
     payload,
+  };
+}
+
+export function setSearchOptions(payload) {
+  return {
+    type: SET_SEARCH_OPTIONS,
+    payload,
+  };
+}
+
+export function setActiveSort(sort, direction) {
+  return {
+    type: SET_ACTIVE_SORT,
+    sort,
+    direction,
   };
 }
