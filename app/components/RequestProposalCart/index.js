a98947f2a4936ecaff47925d3973f0b2cddb0d83
Kosta <utopia2050.kosta@gmail.com>
2016-11-18 19:30:49 +0200

    KIK-877 KIK-879 browser compatibility issue fix on list study page style

--- app/components/RequestProposalCart/index.js
+++ app/components/RequestProposalCart/index.js
@@ -137,9 +137,9 @@ export class RequestProposalCart extends Component {
             <div className="table-holder">
               <table className="table-summary">
                 <colgroup>
-                  <col style={{ width: '44.2%' }} />
-                  <col style={{ width: '17.6%' }} />
-                  <col style={{ width: '23.6%' }} />
+                  <col style={{ width: '36.2%' }} />
+                  <col style={{ width: '19.6%' }} />
+                  <col style={{ width: '25.6%' }} />
                   <col style={{ width: 'auto' }} />
                 </colgroup>
                 <thead>
