ca588527c55750b57e180cd0027eb4d8a6cf4460
Thomas Asenov <thomas.asenov85729@gmail.com>
2016-12-01 17:32:05 +0200

    Fix styles of hover actions

--- app/components/TopHeaderBar/index.js
+++ app/components/TopHeaderBar/index.js
@@ -85,17 +85,18 @@ class TopHeaderBar extends React.Component { // eslint-disable-line react/prefer
           <NotificationBox currentUser={this.props.currentUser} />
 
           <div className="emails pull-left">
-            <a className="opener" data-toggle="tooltip" data-placement="bottom" title="Coming Soon">
+            <a href="#" className="opener" data-toggle="tooltip" data-placement="bottom" title="Coming Soon">
               <i className="icomoon-envelop" />
               <span className="counter">1</span>
             </a>
           </div>
 
           <div className="open-close help-drop pull-left">
-            <a className="link-help pull-left opener">?</a>
+            <a href="#" className="link-help pull-left opener">?</a>
           </div>
 
           <a
+            href="#"
             className={classNames('opener lightbox-opener pull-left btn-chat-popup', { active: this.state.showGlobalPMSModal })}
             onClick={this.showGlobalPMSModal}
           >
