3804ce9bda551de6d6ee30e66b693a17f74911de
Mike Li <mike@instataskapp.com>
2016-12-02 23:22:57 -0800

    KIK-907

--- app/containers/StudyPage/StudyStats.js
+++ app/containers/StudyPage/StudyStats.js
@@ -41,36 +41,36 @@ class StudyStats extends React.Component {
             <Col xs={4}>
               <div className="box same-height-left">
                 <i className="icomoon-open-eye" />
-                <strong className="number">{stats.views}</strong>
+                <strong className="number">{stats.views ? stats.views : 0}</strong>
                 <h3>TOTAL STUDY VIEWS</h3>
               </div>
               <div className="box same-height-left">
                 <i className="icomoon-user-in" />
-                <strong className="number">{stats.referrals}</strong>
+                <strong className="number">{stats.referrals ? stats.referrals : 0}</strong>
                 <h3>TOTAL PATIENT REFERRALS</h3>
               </div>
             </Col>
             <Col xs={4} className="green">
               <div className="box">
                 <i className="icomoon-phone" />
-                <strong className="number">{stats.calls}</strong>
+                <strong className="number">{stats.calls ? stats.calls : 0}</strong>
                 <h3>CALLS PLACED</h3>
               </div>
               <div className="box">
                 <i className="icomoon-icon_clock_alt" />
-                <strong className="number">{stats.callsDuration}</strong>
+                <strong className="number">{stats.callsDuration ? stats.callsDuration : 0}</strong>
                 <h3>CALLS DURATION</h3>
               </div>
             </Col>
             <Col xs={4} className="orange">
               <div className="box same-height-right">
                 <i className="icomoon-chat-up" />
-                <strong className="number">{stats.textsSent}</strong>
+                <strong className="number">{stats.textsSent ? stats.textsSent : 0}</strong>
                 <h3>TEXTS SENT</h3>
               </div>
               <div className="box same-height-right">
                 <i className="icomoon-chat-down" />
-                <strong className="number">{stats.textsReceived}</strong>
+                <strong className="number">{stats.textsReceived ? stats.textsReceived : 0}</strong>
                 <h3>TEXTS RECEIVED</h3>
               </div>
             </Col>
