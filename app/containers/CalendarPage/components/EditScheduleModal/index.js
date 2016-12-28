b613499f68cd6903150e602b31cc7bececca80a0
Mike Li <mike@instataskapp.com>
2016-12-28 15:06:15 -0800

    KIK-1071 Updated styles and modals

--- app/containers/CalendarPage/components/EditScheduleModal/index.js
+++ app/containers/CalendarPage/components/EditScheduleModal/index.js
@@ -7,13 +7,12 @@ import moment from 'moment';
 
 import { SchedulePatientModalType } from 'common/constants';
 
-import ReactSelect from 'components/Input/ReactSelect';
-import DatePicker from 'components/Input/DatePicker';
-import Checkbox from 'components/Input/Checkbox';
+import ReactSelect from '../../../../components/Input/ReactSelect';
+import DatePicker from '../../../../components/Input/DatePicker';
+import Checkbox from '../../../../components/Input/Checkbox';
+import CenteredModal from '../../../../components/CenteredModal';
 
 import validator from './validator';
-
-import './styles.less';
 
 function getTimeComponents(strTime) {
   return {
@@ -62,106 +61,103 @@ export default class EditScheduleModal extends Component {
       periodOptions,
     } = this.props;
 
-    return (
-      <Modal show={modalType === SchedulePatientModalType.UPDATE} onHide={handleCloseModal} id="edit-schedule-modal">
-        { modalType === SchedulePatientModalType.UPDATE &&
-          (<div id="edit-schedule" className="lightbox lightbox-active fixed-popup">
-            <div className="lightbox-holder">
-              <div className="lightbox-frame">
-                <div className="modal-content">
-                  <div className="head">
-                    <strong className="title">EDIT SCHEDULE</strong>
-                    <a className="close lightbox-close" onClick={handleCloseModal}><i className="icomoon-icon_close" /></a>
-                  </div>
-                  <div className="scroll-holder">
-                    <form action="#" className="form-lightbox form-edit-schedule" onSubmit={handleSubmit}>
-                      <strong className="name">{`${selectedCellInfo.data.patient.firstName} ${selectedCellInfo.data.patient.lastName}`}</strong>
-                      <span className="site-location">{selectedCellInfo.data.siteLocation}</span>
-                      <span className="protocol">{selectedCellInfo.data.protocolNumber}</span>
-                      <div className="field-row">
-                        <strong className="label">* When</strong>
-                        <div className="field append-calendar">
-                          <Field
-                            id="start-date"
-                            name="date"
-                            component={DatePicker}
-                            className="form-control datepicker-input"
-                            initialDate={moment(this.props.selectedCellInfo.data.time)}
-                          />
-                        </div>
-                      </div>
-                      <div className="field-row">
-                        <strong className="label required"><label htmlFor="patient-time-edit">Time</label></strong>
-                        <div className="field">
-                          <div className="col-holder row">
-                            <div className="col pull-left hours">
-                              <Field
-                                id="patient-time-edit"
-                                name="hour"
-                                component={ReactSelect}
-                                placeholder="Hours"
-                                options={hourOptions}
-                                className="visible-first-del min-height"
-                                disabled={submitting}
-                              />
-                            </div>
-                            <div className="col pull-left minutes">
-                              <Field
-                                id="minutes2"
-                                name="minute"
-                                component={ReactSelect}
-                                placeholder="Minutes"
-                                options={minuteOptions}
-                                className="visible-first-del min-height"
-                                disabled={submitting}
-                              />
-                            </div>
-                            <div className="col pull-left time-mode">
-                              <Field
-                                id="time-period2"
-                                name="period"
-                                component={ReactSelect}
-                                placeholder="AM/PM"
-                                options={periodOptions}
-                                className="visible-first"
-                                disabled={submitting}
-                              />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="field-row">
-                        <strong className="label">&nbsp;</strong>
-                        <Field
-                          id="text-reminder"
-                          name="textReminder"
-                          component={Checkbox}
-                          type="checkbox"
-                        />
-                        <label className="text-reminder-label" htmlFor="text-reminder">Text Reminder</label>
-                      </div>
-                      <div className="btn-block text-right">
-                        <input
-                          type="button"
-                          className="btn btn-gray-outline lightbox-opener"
-                          disabled={submitting}
-                          value={submitting ? 'deleting...' : 'delete'}
-                          onClick={() => handleDelete(selectedCellInfo.data.id)}
-                        />
-                        <input
-                          type="submit"
-                          className="btn btn-default btn-update"
-                          value={submitting ? 'updating...' : 'update'}
-                        />
-                      </div>
-                    </form>
+    if (modalType === SchedulePatientModalType.UPDATE) {
+      return (
+        <Modal dialogComponentClass={CenteredModal} show={modalType === SchedulePatientModalType.UPDATE}
+               onHide={handleCloseModal} id="edit-schedule-modal">
+          <Modal.Header>
+            <Modal.Title>EDIT SCHEDULE</Modal.Title>
+            <a className="lightbox-close close" onClick={handleCloseModal}>
+              <i className="icomoon-icon_close"/>
+            </a>
+          </Modal.Header>
+          <Modal.Body>
+            <form className="form-lightbox form-edit-schedule" onSubmit={handleSubmit}>
+              <strong
+                className="name">{`${selectedCellInfo.data.patient.firstName} ${selectedCellInfo.data.patient.lastName}`}</strong>
+              <span className="site-location">{selectedCellInfo.data.siteLocation}</span>
+              <span className="protocol">{selectedCellInfo.data.protocolNumber}</span>
+              <div className="field-row">
+                <strong className="label">* When</strong>
+                <div className="field append-calendar">
+                  <Field
+                    id="start-date"
+                    name="date"
+                    component={DatePicker}
+                    className="form-control datepicker-input"
+                    initialDate={moment(this.props.selectedCellInfo.data.time)}
+                  />
+                </div>
+              </div>
+              <div className="field-row">
+                <strong className="label required"><label htmlFor="patient-time-edit">Time</label></strong>
+                <div className="field">
+                  <div className="col-holder row">
+                    <div className="col pull-left hours">
+                      <Field
+                        id="patient-time-edit"
+                        name="hour"
+                        component={ReactSelect}
+                        placeholder="Hours"
+                        options={hourOptions}
+                        className="visible-first-del min-height"
+                        disabled={submitting}
+                      />
+                    </div>
+                    <div className="col pull-left minutes">
+                      <Field
+                        id="minutes2"
+                        name="minute"
+                        component={ReactSelect}
+                        placeholder="Minutes"
+                        options={minuteOptions}
+                        className="visible-first-del min-height"
+                        disabled={submitting}
+                      />
+                    </div>
+                    <div className="col pull-left time-mode">
+                      <Field
+                        id="time-period2"
+                        name="period"
+                        component={ReactSelect}
+                        placeholder="AM/PM"
+                        options={periodOptions}
+                        className="visible-first"
+                        disabled={submitting}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>)
-        }
-      </Modal>
-    );
+              <div className="field-row">
+                <strong className="label">&nbsp;</strong>
+                <Field
+                  id="text-reminder"
+                  name="textReminder"
+                  component={Checkbox}
+                  type="checkbox"
+                />
+                <label className="text-reminder-label" htmlFor="text-reminder">Text Reminder</label>
+              </div>
+              <div className="btn-block text-right">
+                <input
+                  type="button"
+                  className="btn btn-gray-outline lightbox-opener"
+                  disabled={submitting}
+                  value={submitting ? 'deleting...' : 'delete'}
+                  onClick={() => handleDelete(selectedCellInfo.data.id)}
+                />
+                <input
+                  type="submit"
+                  className="btn btn-default btn-update"
+                  value={submitting ? 'updating...' : 'update'}
+                />
+              </div>
+            </form>
+          </Modal.Body>
+        </Modal>
+      );
+    }
+    return null;
   }
 }
