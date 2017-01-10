2420c7f8b54e0fa734b382a90ee9c6fd61f335a7
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2017-01-10 07:55:16 -0400

    changes for credits counter

--- app/containers/StudyPage/sagas.js
+++ app/containers/StudyPage/sagas.js
@@ -571,7 +571,7 @@ function* submitPatientText() {
 function* submitTextBlast() {
   while (true) {
     // listen for the SUBMIT_TEXT_BLAST action
-    const { patients, message, onClose } = yield take(SUBMIT_TEXT_BLAST);
+    const { patients, message, currentUserId, onClose } = yield take(SUBMIT_TEXT_BLAST);
     const authToken = getItem('auth_token');
     if (!authToken) {
       return;
@@ -584,6 +584,7 @@ function* submitTextBlast() {
           patientsIDs: patients.map(patient => (
             patient.id
           )),
+          currentUserId,
           message,
         }),
       });
