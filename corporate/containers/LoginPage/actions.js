f90f37b8827d8c612fedbdc30c6491926ad34416
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2017-01-19 16:23:18 +0200

    login page using reduxForm

--- corporate/containers/LoginPage/actions.js
+++ corporate/containers/LoginPage/actions.js
@@ -2,8 +2,8 @@ import {
   LOGIN_REQUEST,
 } from './constants';
 
+// TODO: all actions from the app/components/LoginPage must be transferred here
 export function loginRequest(payload) {
-  console.log('payload', payload);
   return {
     type: LOGIN_REQUEST,
     payload,
