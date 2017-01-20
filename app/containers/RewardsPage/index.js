6d822f2f4dfcdaadcd1463b5b88ce27a3c068ccf
Fabian Bigos <fabianbigos2015@outlook.com>
2017-01-20 18:03:11 +0100

    fix rewards page styles

--- app/containers/RewardsPage/index.js
+++ app/containers/RewardsPage/index.js
@@ -160,8 +160,8 @@ export class RewardsPage extends React.Component { // eslint-disable-line react/
             </aside>
 
             <div className="detail">
-              <div className="infoarea row">
-                <div className="col-sm-3 column diamond">
+              <div className="infoarea row table-parent">
+                <div className="col-sm-3 column diamond table-child">
                   <div>
                     <div className="box">
                       <div className="box-holder">
@@ -180,7 +180,7 @@ export class RewardsPage extends React.Component { // eslint-disable-line react/
                   <div className="package-img diamond"><img className="ruby-img" src={ruby} alt="RUBY LISTING" width="135" /></div>
                 </div>
 
-                <div className="col-sm-3 column">
+                <div className="col-sm-3 column table-child">
                   <div>
                     <div className="box">
                       <div className="box-holder">
@@ -199,7 +199,7 @@ export class RewardsPage extends React.Component { // eslint-disable-line react/
                   <div className="package-img diamond"><img src={diamond} alt="DIAMOND LISTING" width="115" height="102" /></div>
                 </div>
 
-                <div className="col-sm-3 msg-info column">
+                <div className="col-sm-3 msg-info column table-child">
                   <div>
                     <div className="box">
                       <div className="box-holder">
@@ -218,7 +218,7 @@ export class RewardsPage extends React.Component { // eslint-disable-line react/
                   <div className="package-img platinum"><img src={platinum} alt="PLATINUM LISTING" width="108" height="115" /></div>
                 </div>
 
-                <div className="col-sm-3 rewards-info column">
+                <div className="col-sm-3 rewards-info column table-child">
                   <div>
                     <div className="box">
                       <div className="box-holder">
