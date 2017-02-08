a37760ec7681d476148912ec7af197e21d4b422d
coolstudykik <david.jansen@studykik.com>
2017-02-08 17:56:01 +0100

    KIK-1187, fix styles and validation

--- app/components/ClientSitesList/ClientSiteItem.js
+++ app/components/ClientSitesList/ClientSiteItem.js
@@ -81,7 +81,7 @@ class ClientSiteItem extends Component { // eslint-disable-line react/prefer-sta
         <span className="edit-assigned-user">
           {(this.assignedUserIsBeingFetched(item))
             ? <span><LoadingSpinner showOnlyIcon size={20} className="fetching-assigned-user" /></span>
-            : <a className="btn edit-icon" onClick={() => { this.editAssignedUser(item); }}><i className="pencil-square" /></a>
+            : <a className="btn toggle edit-icon" onClick={() => { this.editAssignedUser(item); }}><i className="pencil-square" /></a>
           }
         </span>
       </div>
