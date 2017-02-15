224dd8b06acc2f7b6b68b32f6ffd543acc8bcd2d
Mike Li <mike@instataskapp.com>
2017-02-15 14:01:40 -0800

    Updates helper method for sanitizing props. Updates modals to validate add patient forms better.

--- app/containers/StudyPage/ImportPatients/validator.js
+++ app/containers/StudyPage/ImportPatients/validator.js
@@ -3,7 +3,14 @@ import { validatorFactory } from '../../../utils/reduxForm';
 const schema = {
   firstName: { presence: { message: '^First name cannot be blank' } },
   lastName: { presence: { message: '^Last name cannot be blank' } },
-  email: { email: { message: '^Email not valid' } },
+  email: {
+    presence: {
+      message: '^Email cannot be blank',
+    },
+    email: {
+      message: '^Email not valid',
+    },
+  },
   phone: {
     presence: {
       message: '^Phone number cannot be blank',
@@ -16,7 +23,10 @@ const schema = {
       message: '^Phone number too short',
     },
   },
-
 };
+
+const fields = Object.keys(schema);
+
+export { fields };
 
 export default validatorFactory(schema);
