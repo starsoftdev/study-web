3522d5b66da620acb7f81db404de4e1a2b946dc4
coolstudykik <david.jansen@studykik.com>
2017-02-21 19:21:00 +0100

    KIK-1187, fix textblast and upgrade validation

--- app/containers/HomePage/UpgradeStudyForm/validator.js
+++ app/containers/HomePage/UpgradeStudyForm/validator.js
@@ -3,8 +3,7 @@ export { upgradeStudyFields };
 
 export default values => {
   const errors = {};
-
-  if (!values.level && !values.addPatientMessagingSuite && !values.addPatientQualificationSuite) {
+  if ((!values.level && !values.addPatientMessagingSuite && !values.addPatientQualificationSuite) || !values.validateToggle) {
     errors.level = 'You need to select either Upgrade Level, Patient Messaging Suite, or Patient Qualification Suite';
     errors.addPatientMessagingSuite = 'You need to select either Upgrade Level, Patient Messaging Suite, or Patient Qualification Suite';
     errors.addPatientQualificationSuite = 'You need to select either Upgrade Level, Patient Messaging Suite, or Patient Qualification Suite';
