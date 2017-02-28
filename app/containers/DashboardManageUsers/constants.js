fc14eeae7d8d1d7008d2b9e9ae422a9306abe8c2
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2017-02-28 11:25:04 +0200

    KIK-1282. Dashboard Manage Users functionality.

--- app/containers/DashboardManageUsers/constants.js
+++ app/containers/DashboardManageUsers/constants.js
@@ -4,4 +4,20 @@
  *
  */
 
-export const DEFAULT_ACTION = 'app/DashboardManageUsers/DEFAULT_ACTION';
+export const FETCH_ADMINS = 'app/DashboardManageUsers/FETCH_ADMINS';
+export const FETCH_ADMINS_SUCCESS = 'app/DashboardManageUsers/FETCH_ADMINS_SUCCESS';
+export const FETCH_ADMINS_ERROR = 'app/DashboardManageUsers/FETCH_ADMINS_ERROR';
+
+export const FETCH_ADMIN_ROLES = 'app/DashboardManageUsers/FETCH_ADMIN_ROLES';
+export const FETCH_ADMIN_ROLES_SUCCESS = 'app/DashboardManageUsers/FETCH_ADMIN_ROLES_SUCCESS';
+export const FETCH_ADMIN_ROLES_ERROR = 'app/DashboardManageUsers/FETCH_ADMIN_ROLES_ERROR';
+
+export const EDIT_DASHBOARD_USER = 'app/DashboardManageUsers/EDIT_DASHBOARD_USER';
+export const EDIT_DASHBOARD_USER_SUCCESS = 'app/DashboardManageUsers/EDIT_DASHBOARD_USER_SUCCESS';
+export const EDIT_DASHBOARD_USER_ERROR = 'app/DashboardManageUsers/EDIT_DASHBOARD_USER_ERROR';
+
+export const DELETE_DASHBOARD_USER = 'app/DashboardManageUsers/DELETE_DASHBOARD_USER';
+export const DELETE_DASHBOARD_USER_SUCCESS = 'app/DashboardManageUsers/DELETE_DASHBOARD_USER_SUCCESS';
+export const DELETE_DASHBOARD_USER_ERROR = 'app/DashboardManageUsers/DELETE_DASHBOARD_USER_ERROR';
+
+export const SET_ACTIVE_SORT = 'app/DashboardManageUsers/SET_ACTIVE_SORT';
