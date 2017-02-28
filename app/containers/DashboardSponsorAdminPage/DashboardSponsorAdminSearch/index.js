a502a44fcf9f4cb9305c0407803659efc22c9cc3
Dennis Kroon <denkroon84@gmail.com>
2017-02-28 17:28:53 +0100

    fix eslint

--- app/containers/DashboardSponsorAdminPage/DashboardSponsorAdminSearch/index.js
+++ app/containers/DashboardSponsorAdminPage/DashboardSponsorAdminSearch/index.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import Button from 'react-bootstrap/lib/Button';
 import { Field, reduxForm } from 'redux-form';
-import { map } from 'lodash';
 import Modal from 'react-bootstrap/lib/Modal';
 import Input from '../../../components/Input';
 import CenteredModal from '../../../components/CenteredModal/index';
@@ -47,12 +46,12 @@ export class DashboardSponsorAdminSearch extends React.Component {
   }
 
   render() {
-    const options = [
-      { id: 1, name: 'Bruce Wayne', company: 'Pfizer', email: 'bruce.wayne@wayneenterprise.com', bd: 'Kobe Byant', ae: 'Michael Grimm' },
-      { id: 2, name: 'Ray Palmer', company: 'Company 1', email: 'ray.palmer@palmertech.com', bd: 'Bianca Ryan', ae: 'Cas Haley' },
-      { id: 3, name: 'Will Graham', company: 'Company 2', email: 'will.graham@wayneenterprise.com', bd: 'Terry Fator', ae: 'Eli Mattson' },
-      { id: 4, name: 'Jon Snow', company: 'Company 3', email: 'jon.snow@wayneenterprise.com', bd: 'Kevin Skinner', ae: 'Tom Cotter' },
-    ];
+    // const options = [
+    //   { id: 1, name: 'Bruce Wayne', company: 'Pfizer', email: 'bruce.wayne@wayneenterprise.com', bd: 'Kobe Byant', ae: 'Michael Grimm' },
+    //   { id: 2, name: 'Ray Palmer', company: 'Company 1', email: 'ray.palmer@palmertech.com', bd: 'Bianca Ryan', ae: 'Cas Haley' },
+    //   { id: 3, name: 'Will Graham', company: 'Company 2', email: 'will.graham@wayneenterprise.com', bd: 'Terry Fator', ae: 'Eli Mattson' },
+    //   { id: 4, name: 'Jon Snow', company: 'Company 3', email: 'jon.snow@wayneenterprise.com', bd: 'Kevin Skinner', ae: 'Tom Cotter' },
+    // ];
 
     return (
       <form action="#" className="form-search clearfix">
