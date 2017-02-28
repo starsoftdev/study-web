d25e40dbd3b53c8429ba40291a3ba5a9ea9ce33a
Dennis Kroon <denkroon84@gmail.com>
2017-02-28 21:06:11 +0100

    implement dropdown list for sponsor, ae and bd, fix typo

--- app/containers/DashboardSponsorAdminPage/DashboardSponsorAdminSearch/AddSponsorAdminForm.js
+++ app/containers/DashboardSponsorAdminPage/DashboardSponsorAdminSearch/AddSponsorAdminForm.js
@@ -1,17 +1,61 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { map, find } from 'lodash';
 import { Field, reduxForm } from 'redux-form';
 import Input from '../../../components/Input';
+import ReactMultiSelect from '../../../components/Input/ReactMultiSelect';
 
 @reduxForm({ form: 'dashboardAddSponsorAdminForm' })
 
 export class AddSponsorAdminForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
     isEdit: PropTypes.bool,
+    sponsorData: PropTypes.array,
+    initialValues: PropTypes.object,
   }
 
   render() {
+    const itemTemplate = (controlSelectedValue) => (
+      <div key={controlSelectedValue.value}>
+        {controlSelectedValue.label}
+        <i className="close-icon icomoon-icon_close" />
+      </div>
+    );
+
+    const selectedItemsTemplate = (controlSelectedValue) => (
+      <div>
+        {controlSelectedValue[0].value}
+      </div>
+    );
+
+    const sponsors = map(this.props.sponsorData, (sponsor) => ({
+      id: sponsor.id,
+      label: sponsor.company,
+      value: sponsor.company,
+    }));
+
+    const bds = map(this.props.sponsorData, (sponsor) => ({
+      id: sponsor.id,
+      label: sponsor.bd,
+      value: sponsor.bd,
+    }));
+
+    const aes = map(this.props.sponsorData, (sponsor) => ({
+      id: sponsor.id,
+      label: sponsor.ae,
+      value: sponsor.ae,
+    }));
+    let initialSponsor;
+    let initialBD;
+    let initialAE;
+
+    if (this.props.isEdit) {
+      initialSponsor = find(sponsors, { label: this.props.initialValues.company });
+      initialBD = find(bds, { label: this.props.initialValues.bd });
+      initialAE = find(aes, { label: this.props.initialValues.ae });
+    }
+
     return (
       <form action="#" className="form-lightbox dashboard-lightbox">
 
@@ -22,9 +66,17 @@ export class AddSponsorAdminForm extends React.Component { // eslint-disable-lin
           <div className="field">
             <Field
               name="company"
-              component={Input}
-              type="text"
+              component={ReactMultiSelect}
               placeholder="Select Sponsor"
+              searchPlaceholder="Search"
+              initialValue={initialSponsor}
+              searchable
+              optionLabelKey="label"
+              onChange={(e) => console.log('init search', e)}
+              customOptionTemplateFunction={itemTemplate}
+              customSelectedValueTemplateFunction={selectedItemsTemplate}
+              dataSource={sponsors}
+              customSearchIconClass="icomoon-icon_search2"
             />
           </div>
         </div>
@@ -76,8 +128,17 @@ export class AddSponsorAdminForm extends React.Component { // eslint-disable-lin
               <div className="field">
                 <Field
                   name="bd"
-                  component={Input}
-                  type="text"
+                  component={ReactMultiSelect}
+                  placeholder="Select BD"
+                  searchPlaceholder="Search"
+                  initialValue={initialBD}
+                  searchable
+                  optionLabelKey="label"
+                  onChange={(e) => console.log('init search', e)}
+                  customOptionTemplateFunction={itemTemplate}
+                  customSelectedValueTemplateFunction={selectedItemsTemplate}
+                  dataSource={bds}
+                  customSearchIconClass="icomoon-icon_search2"
                 />
               </div>
             </div>
@@ -88,8 +149,16 @@ export class AddSponsorAdminForm extends React.Component { // eslint-disable-lin
               <div className="field">
                 <Field
                   name="ae"
-                  component={Input}
-                  type="text"
+                  component={ReactMultiSelect}
+                  placeholder="Select AE"
+                  searchPlaceholder="Search"
+                  initialValue={initialAE}
+                  searchable
+                  optionLabelKey="label"
+                  customOptionTemplateFunction={itemTemplate}
+                  customSelectedValueTemplateFunction={selectedItemsTemplate}
+                  dataSource={aes}
+                  customSearchIconClass="icomoon-icon_search2"
                 />
               </div>
             </div>
