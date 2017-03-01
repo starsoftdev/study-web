66758b2ad11866df3ffe5c215a92fe346d6f310e
Mike Li <mike@instataskapp.com>
2017-02-28 20:09:12 -0800

    Fix eslint error.

--- app/containers/DashboardSponsorAdminPage/DashboardSponsorAdminTable/index.js
+++ app/containers/DashboardSponsorAdminPage/DashboardSponsorAdminTable/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { map } from 'lodash';
 import { createStructuredSelector } from 'reselect';
 import RowItem from './RowItem';
 
