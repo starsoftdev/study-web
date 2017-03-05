cbaad2e9e32f3c63db2b0c32c78f2648cc8a106d
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2017-03-05 14:48:13 +0200

    KIK-1334. Dashboard cro and protocols pages.

--- app/containers/DashboardCROPage/index.js
+++ app/containers/DashboardCROPage/index.js
@@ -4,31 +4,72 @@
  *
  */
 
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import selectDashboardCROPage from './selectors';
+import { createStructuredSelector } from 'reselect';
 
 import { DashboardCROSearch } from './DashboardCROSearch/index';
 import { DashboardCROTable } from './DashboardCROTable';
 
+import { fetchCro, addCro, editCro, deleteCro, setActiveSort } from './actions';
+import { selectDashboardCro, selectDashboardEditCroProcess, selectDashboardCroSearchFormValues, selectPaginationOptions } from './selectors';
+
 export class DashboardCROPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  static propTypes = {
+    fetchCro: PropTypes.func,
+    cro: PropTypes.object,
+    addCro: PropTypes.func,
+    editCro: PropTypes.func,
+    deleteCro: PropTypes.func,
+    setActiveSort: PropTypes.func,
+    editCroProcess: PropTypes.object,
+    croSearchFormValues: PropTypes.object,
+    paginationOptions: PropTypes.object,
+  }
+
+  componentWillMount() {
+    this.props.fetchCro();
+  }
+
   render() {
     return (
       <div className="container-fluid dashboard-cro">
         <h2 className="main-heading">CRO</h2>
 
-        <DashboardCROSearch />
-        <DashboardCROTable />
+        <DashboardCROSearch
+          cro={this.props.cro}
+          addCro={this.props.addCro}
+          editCroProcess={this.props.editCroProcess}
+        />
+        <DashboardCROTable
+          cro={this.props.cro}
+          editCroProcess={this.props.editCroProcess}
+          editCro={this.props.editCro}
+          deleteCro={this.props.deleteCro}
+          croSearchFormValues={this.props.croSearchFormValues}
+          setActiveSort={this.props.setActiveSort}
+          paginationOptions={this.props.paginationOptions}
+        />
       </div>
     );
   }
 }
 
-const mapStateToProps = selectDashboardCROPage();
+const mapStateToProps = createStructuredSelector({
+  cro: selectDashboardCro(),
+  editCroProcess: selectDashboardEditCroProcess(),
+  croSearchFormValues: selectDashboardCroSearchFormValues(),
+  paginationOptions: selectPaginationOptions(),
+});
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
+    fetchCro: () => dispatch(fetchCro()),
+    addCro: (payload) => dispatch(addCro(payload)),
+    editCro: (payload) => dispatch(editCro(payload)),
+    deleteCro: (payload) => dispatch(deleteCro(payload)),
+    setActiveSort: (sort, direction) => dispatch(setActiveSort(sort, direction)),
   };
 }
 
--- app/containers/DashboardProtocolPage/constants.js
+++ app/containers/DashboardProtocolPage/constants.js
@@ -3,5 +3,20 @@
  * DashboardProtocolPage constants
  *
  */
+export const FETCH_PROTOCOL = 'app/DashboardProtocolPage/FETCH_PROTOCOL';
+export const FETCH_PROTOCOL_SUCCESS = 'app/DashboardProtocolPage/FETCH_PROTOCOL_SUCCESS';
+export const FETCH_PROTOCOL_ERROR = 'app/DashboardProtocolPage/FETCH_PROTOCOL_ERROR';
 
-export const DEFAULT_ACTION = 'app/DashboardProtocolPage/DEFAULT_ACTION';
+export const ADD_PROTOCOL = 'app/DashboardProtocolPage/ADD_PROTOCOL';
+export const ADD_PROTOCOL_SUCCESS = 'app/DashboardProtocolPage/ADD_PROTOCOL_SUCCESS';
+export const ADD_PROTOCOL_ERROR = 'app/DashboardProtocolPage/ADD_PROTOCOL_ERROR';
+
+export const EDIT_PROTOCOL = 'app/DashboardProtocolPage/EDIT_PROTOCOL';
+export const EDIT_PROTOCOL_SUCCESS = 'app/DashboardProtocolPage/EDIT_PROTOCOL_SUCCESS';
+export const EDIT_PROTOCOL_ERROR = 'app/DashboardProtocolPage/EDIT_PROTOCOL_ERROR';
+
+export const DELETE_PROTOCOL = 'app/DashboardProtocolPage/DELETE_PROTOCOL';
+export const DELETE_PROTOCOL_SUCCESS = 'app/DashboardProtocolPage/DELETE_PROTOCOL_SUCCESS';
+export const DELETE_PROTOCOL_ERROR = 'app/DashboardProtocolPage/DELETE_PROTOCOL_ERROR';
+
+export const SET_ACTIVE_SORT = 'app/DashboardProtocolPage/SET_ACTIVE_SORT';
