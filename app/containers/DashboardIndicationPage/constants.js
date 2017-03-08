ebdc984e632907e603d5eec8ca15e20a93471327
coolstudykik <david.jansen@studykik.com>
2017-03-08 16:33:21 +0100

    KIK-1347, fix bug for Sponsor Portal

--- app/containers/DashboardIndicationPage/constants.js
+++ app/containers/DashboardIndicationPage/constants.js
@@ -7,3 +7,13 @@
 export const FETCH_INDICATIONS = 'app/DashboardIndicationPage/FETCH_INDICATIONS';
 export const FETCH_INDICATIONS_SUCCESS = 'app/DashboardIndicationPage/FETCH_INDICATIONS_SUCCESS';
 export const FETCH_INDICATIONS_ERROR = 'app/DashboardIndicationPage/FETCH_INDICATIONS_ERROR';
+export const ADD_INDICATION = 'app/DashboardIndicationPage/ADD_INDICATION';
+export const ADD_INDICATION_SUCCESS = 'app/DashboardIndicationPage/ADD_INDICATION_SUCCESS';
+export const ADD_INDICATION_ERROR = 'app/DashboardIndicationPage/ADD_INDICATION_ERROR';
+export const EDIT_INDICATION = 'app/DashboardIndicationPage/EDIT_INDICATION';
+export const EDIT_INDICATION_SUCCESS = 'app/DashboardIndicationPage/EDIT_INDICATION_SUCCESS';
+export const EDIT_INDICATION_ERROR = 'app/DashboardIndicationPage/EDIT_INDICATION_ERROR';
+export const DELETE_INDICATION = 'app/DashboardIndicationPage/DELETE_INDICATION';
+export const DELETE_INDICATION_SUCCESS = 'app/DashboardIndicationPage/DELETE_INDICATION_SUCCESS';
+export const DELETE_INDICATION_ERROR = 'app/DashboardIndicationPage/DELETE_INDICATION_ERROR';
+export const SET_ACTIVE_SORT = 'app/DashboardIndicationPage/SET_ACTIVE_SORT';
