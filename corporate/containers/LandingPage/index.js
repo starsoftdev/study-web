66ef17df1dea5ab136972548e8d90bd657ddb25e
ihor <vertical.bitrix@gmail.com>
2017-03-14 20:53:37 +0200

    add lastname for thank you email

--- corporate/containers/LandingPage/index.js
+++ corporate/containers/LandingPage/index.js
@@ -110,7 +110,7 @@ export class LandingPage extends React.Component {
       unsubscribed: false,
       study_id: landing.id,
       study_source_id: studySourceId,
-      indicationName: landing.indication.name
+      indicationName: landing.indication.name,
     };
 
     this.props.subscribeFromLanding(data);
