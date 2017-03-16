f80af369e5019785882ed37d901aab1a63c4ad38
coolstudykik <david.jansen@studykik.com>
2017-03-16 17:32:30 +0100

    KIK-1372, exposure level dashboard

--- app/components/TopHeaderBar2/AvatarMenu/index.js
+++ app/components/TopHeaderBar2/AvatarMenu/index.js
@@ -47,6 +47,7 @@ class AvatarMenu extends React.Component {
           <div className="well">
             <ul className="list-unstyled">
               <li><Link to="/me/profile" onClick={() => this.handleClickOutside()}>PROFILE</Link></li>
+              <li><Link to="/app/dashboard-exposure-level" onClick={() => this.handleClickOutside()}>EXPOSURE LEVEL</Link></li>
               <li><Link to="/app/dashboard-indication" onClick={() => this.handleClickOutside()}>INDICATION</Link></li>
               <li><Link to="/app/dashboard-protocol" onClick={() => this.handleClickOutside()}>Protocol</Link></li>
               <li><Link to="/app/dashboard-cro" onClick={() => this.handleClickOutside()}>CRO</Link></li>
--- app/routes.js
+++ app/routes.js
@@ -628,6 +628,28 @@ export default function createRoutes(store) {
       },
     }, {
       onEnter: redirectToLogin,
+      path: '/app/dashboard-exposure-level',
+      name: 'dashboardExposureLevelPage',
+      getComponent(nextState, cb) {
+        const importModules = Promise.all([
+          System.import('./containers/DashboardExposureLevelPage/reducer'),
+          System.import('./containers/DashboardExposureLevelPage/sagas'),
+          System.import('./containers/DashboardExposureLevelPage'),
+        ]);
+
+        const renderRoute = loadModule(cb);
+
+        importModules.then(([reducer, sagas, component]) => {
+          injectReducer('dashboardExposureLevelPage', reducer.default);
+          injectReducer('dashboardExposureLevelPage', reducer.default);
+          injectSagas(sagas.default);
+          renderRoute(component);
+        });
+
+        importModules.catch(errorLoading);
+      },
+    }, {
+      onEnter: redirectToLogin,
       path: '/app/dashboard-indication',
       name: 'dashboardIndicationPage',
       getComponent(nextState, cb) {
