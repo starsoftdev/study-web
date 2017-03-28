e165737782f254a0658b770743140e02421893d2
Dennis Kroon <denkroon84@gmail.com>
2017-03-28 17:37:23 +0200

    KIK-1428 fix spinning issue on shopping cart

--- app/components/ShoppingCartForm/index.js
+++ app/components/ShoppingCartForm/index.js
@@ -18,7 +18,6 @@ import ReactSelect from '../../components/Input/ReactSelect';
 import AddNewCardForm from '../../components/AddNewCardForm';
 import { selectCouponId, selectTotal } from './selectors';
 import { selectCoupon, selectCards, selectCurrentUserStripeCustomerId, selectSavedCard } from '../../containers/App/selectors';
-import { selectFormSubmissionStatus } from '../../containers/ListNewStudyPage/selectors';
 import formValidator from './validator';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import Money from '../../components/Money';
@@ -31,7 +30,6 @@ const mapStateToProps = createStructuredSelector({
   cards: selectCards(),
   currentUserStripeCustomerId: selectCurrentUserStripeCustomerId(),
   savedCard: selectSavedCard(),
-  formSubmissionStatus: selectFormSubmissionStatus(),
 });
 
 function mapDispatchToProps(dispatch) {
@@ -67,7 +65,6 @@ class ShoppingCartForm extends Component { // eslint-disable-line react/prefer-s
     validateAndSubmit: PropTypes.func,
     manualDisableSubmit: PropTypes.bool,
     showAddNewCard: PropTypes.func,
-    formSubmissionStatus: PropTypes.object,
   };
 
   constructor(props) {
@@ -80,7 +77,7 @@ class ShoppingCartForm extends Component { // eslint-disable-line react/prefer-s
     this.onSelectCard = this.onSelectCard.bind(this);
     this.state = {
       addNewCardModalOpen: false,
-      showLoading: props.formSubmissionStatus.submitting,
+      showLoading: props.submitting,
     };
   }
 
@@ -100,8 +97,7 @@ class ShoppingCartForm extends Component { // eslint-disable-line react/prefer-s
     if (!newProps.savedCard.saving && this.props.savedCard.saving) {
       this.closeAddNewCardModal();
     }
-
-    this.setState({ showLoading: newProps.formSubmissionStatus.submitting });
+    this.setState({ showLoading : newProps.submitting });
   }
 
   componentWillUnmount() {
@@ -296,7 +292,13 @@ class ShoppingCartForm extends Component { // eslint-disable-line react/prefer-s
               />
             </div>
             {cardsPanelContent}
-            <Button disabled={coupon.fetching || cards.fetching || submitting || manualDisableSubmit} onClick={validateAndSubmit}>
+            <Button
+              disabled={coupon.fetching || cards.fetching || submitting || manualDisableSubmit}
+              onClick={(ev) => {
+                this.setState({ showLoading: true });
+                validateAndSubmit(ev);
+              }}
+            >
               <span>Submit</span>
               {showLoading &&
                 <span className="pull-right"><LoadingSpinner showOnlyIcon size={20} /></span>
--- app/containers/HomePage/StudiesList/index.js
+++ app/containers/HomePage/StudiesList/index.js
@@ -563,6 +563,7 @@ class StudiesList extends Component { // eslint-disable-line react/prefer-statel
               onHide={this.closeRenewModal}
               onShow={this.showRenewModal}
               manualDisableSubmit={this.props.renewedStudy.submitting}
+              submitting={this.props.renewedStudy.submitting}
               validateAndSubmit={this.handleRenewStudyFormSubmit}
               currentUserStripeCustomerId={this.props.currentUserStripeCustomerId}
             />
@@ -572,6 +573,7 @@ class StudiesList extends Component { // eslint-disable-line react/prefer-statel
               onHide={this.closeUpgradeModal}
               onShow={this.showUpgradeModal}
               manualDisableSubmit={this.props.upgradedStudy.submitting}
+              submitting={this.props.upgradedStudy.submitting}
               validateAndSubmit={this.handleUpgradeStudyFormSubmit}
               currentUserStripeCustomerId={this.props.currentUserStripeCustomerId}
             />
