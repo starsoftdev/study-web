af0972a8d6858687cecbb3cc3415a6d203b5bc30
Mike Li <mike@instataskapp.com>
2017-04-05 16:04:22 -0700

    Revert "Refactors redirect phone to just be phone field."
    This reverts commit c65ba748ea6b78c16897f6be55402000ab34083c.

--- app/components/EditSiteForm/validator.js
+++ app/components/EditSiteForm/validator.js
@@ -4,7 +4,7 @@ const schema = {
   name: { presence: true },
   piFirstName: { presence: true },
   piLastName: { presence: true },
-  phone: { presence: true },
+  redirectPhone: { presence: true },
   address: { presence: true },
   city: { presence: true },
   state: { presence: true },
