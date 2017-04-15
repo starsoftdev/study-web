a916feffa8c930126de0b02824b1204209426464
Dennis Kroon <denkroon84@gmail.com>
2017-04-15 04:02:38 +0200

    KIK-1571 implement not found page

--- corporate/containers/NotFoundPage/index.js
+++ corporate/containers/NotFoundPage/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import img1 from '../../assets/images/404.png';
+import img2 from '../../assets/images/img20.png';
+
+
 export default class NotFound extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   static propTypes = {};
@@ -10,8 +14,38 @@ export default class NotFound extends React.Component { // eslint-disable-line r
 
   render() {
     return (
-      <div id="">
-        NotFound layout will be here soon.
+      <div id="main" className="not-fouund-page">
+        <div className="container">
+          <div className="img-holder">
+            <img src={img1} alt="img4" className="img-responsive" />
+          </div>
+          <h3 className="text-center">The page you found does not Exist.</h3>
+          <h2 className="text-center">HELP DECIDE THE I.T. TEAMS FATE.</h2>
+        </div>
+        <div className="container">
+          <div className="buttons text-center">
+            <a href="/" >
+              <span
+                className="btn-default btn-fire btn"
+                onClick={this.toggleListNow}
+              >
+                FIRE THEM
+              </span>
+            </a>
+            <a href="/" >
+              <span
+                className="btn-default btn-live btn"
+                onClick={this.toggleListNow}
+              >
+                LET THEM LIVE
+              </span>
+            </a>
+          </div>
+          <div className="img-holder complaint_image">
+            <img src={img2} alt="img4" className="img-responsive" />
+          </div>
+        </div>
+        
       </div>
     );
   }
