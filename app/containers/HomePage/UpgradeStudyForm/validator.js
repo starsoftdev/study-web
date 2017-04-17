28a086964371f94993495253cc545d9f30c48215
Mike Li <mike@instataskapp.com>
2017-04-17 16:46:30 -0700

    KIK-1604

--- app/containers/HomePage/UpgradeStudyForm/validator.js
+++ app/containers/HomePage/UpgradeStudyForm/validator.js
@@ -3,9 +3,8 @@ export { upgradeStudyFields };
 
 export default values => {
   const errors = {};
-  if ((!values.level && !values.addPatientMessagingSuite && !values.addPatientQualificationSuite) || !values.validateToggle) {
+  if (!values.level && !values.addPatientQualificationSuite) {
     errors.level = 'You need to select either Upgrade Level, Patient Messaging Suite, or Patient Qualification Suite';
-    errors.addPatientMessagingSuite = 'You need to select either Upgrade Level, Patient Messaging Suite, or Patient Qualification Suite';
     errors.addPatientQualificationSuite = 'You need to select either Upgrade Level, Patient Messaging Suite, or Patient Qualification Suite';
   }
   return errors;
