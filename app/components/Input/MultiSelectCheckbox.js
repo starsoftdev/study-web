70442811cb9f1046f5365bf1d35b510931aeeeaf
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2017-05-19 12:39:43 +0300

    KIK-1870. Manage Users pop up issues and 2 of the same protocol showing.

--- app/components/Input/MultiSelectCheckbox.js
+++ app/components/Input/MultiSelectCheckbox.js
@@ -37,18 +37,6 @@ export default class MultiSelectCheckbox extends Component {
     this.selectAll = this.selectAll.bind(this);
   }
 
-  componentWillMount() {
-    const componentValues = [{ isAdmin: this.props.isAdmin }];
-    _.forEach(this.props.options, (item) => {
-      if (item.value) {
-        componentValues.push(item);
-      }
-    });
-
-    this.props.input.onChange(componentValues);
-    this.props.input.onBlur(componentValues);
-  }
-
   onHideOverlay() {
     this.setState({
       showIndicationPopover: false,
