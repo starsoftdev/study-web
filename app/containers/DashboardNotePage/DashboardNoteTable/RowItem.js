b3cc283de2678beca2fc5f5da0ef1398e4fe60b2
Mike Li <mike@instataskapp.com>
2017-05-25 17:53:50 -0700

    Fix compilation error.

--- app/containers/DashboardNotePage/DashboardNoteTable/RowItem.js
+++ app/containers/DashboardNotePage/DashboardNoteTable/RowItem.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import moment from 'moment-timezone';
 import Modal from 'react-bootstrap/lib/Modal';
-import moment from 'moment-timezone';
 import CenteredModal from '../../../components/CenteredModal/index';
 import { AddNoteForm } from '../DashboardNoteSearch/AddNoteForm';
 
