c6b13fab767d28dee892979c58c1130b25799963
coolstudykik <david.jansen@studykik.com>
2017-06-01 18:10:37 +0200

    KIK-1936, add li

--- app/containers/HomePage/AdminDashboard/StudyList/StudyLeftItem.js
+++ app/containers/HomePage/AdminDashboard/StudyList/StudyLeftItem.js
@@ -104,7 +104,7 @@ class StudyLeftItem extends Component { // eslint-disable-line react/prefer-stat
             <li className="sponsor"><div className="special_ellipsis_div">Sponsor: <span>{item.sponsor_name || 'N/A'}</span></div></li>
             <li className="cro">CRO: <span>{item.cro_name || 'N/A'}</span></li>
             <li className="login-info">Last Login: <span>{lastLoginTime}</span></li>
-            <span><a className="special_ellipsis_link landing-link" onClick={() => { this.showNote(); }}>Note</a></span>
+            <li className="login-info"><span><a className="special_ellipsis_link landing-link" onClick={() => { this.showNote(); }}>Note</a></span></li>
           </ul>
         </td>
         <td>
