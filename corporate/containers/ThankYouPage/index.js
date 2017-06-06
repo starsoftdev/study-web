6401a38b3cdebf9003b32d7842166381acd19382
ihor <vertical.bitrix@gmail.com>
2017-06-06 03:36:39 +0300

    Add conversion pixel to the Thank You page at the bottom of the HTML

--- corporate/containers/ThankYouPage/index.js
+++ corporate/containers/ThankYouPage/index.js
@@ -119,6 +119,25 @@ export class ThankYouPage extends React.Component {
             </div>
           </section>
         </div>
+        <script
+          dangerouslySetInnerHTML={{ __html: `
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '914628025278184'); // Insert your pixel ID here.
+            fbq('track', 'PageView');
+          `}}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style="display:none"
+            src="https://www.facebook.com/tr?id=914628025278184&ev=PageView&noscript=1"
+          />
+        </noscript>
       </div>
     );
   }
