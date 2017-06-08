9e63ed4f7c66416a8b1541f79f78e70487fdcf10
Mike Li <mike@instataskapp.com>
2017-06-08 05:12:41 -0700

    Revert "KIK-2037, removing all filtering code related to fetching studies from reducer. logic was moved to server"
    This reverts commit e86574dc34d413553c5143e0f37a79e7165f2e45.

--- app/containers/HomePage/reducer.js
+++ app/containers/HomePage/reducer.js
@@ -218,10 +218,73 @@ export default function homePageReducer(state = initialState, action) {
         },
       };
     case FETCH_STUDIES_SUCCESS: {
+      const cDate = new Date();
+      // const dateFrom = campaign.dateFrom ? new Date(campaign.dateFrom) : null
+      // const dateTo = campaign.dateTo ? new Date(campaign.dateTo) : null
+      //
+      // const dateFromStr = dateFrom ? moment(dateFrom).format('MMMM Do, YYYY') : 'To Be Determined'
+      // const dateToStr = dateTo ? moment(dateTo).format('MMMM Do, YYYY') : 'To Be Determined'
+      const entitiesCollection = payload.studies.map((studyObject, index) => ({
+        studyId: studyObject.id,
+        indication: studyObject.indication,
+        siteName: studyObject.site.siteName,
+        siteTimezone: studyObject.site.timezone,
+        sponsor: studyObject.sponsor.name,
+        protocol: studyObject.protocolNumber,
+        patientMessagingSuite: studyObject.patientMessagingSuite ? 'On' : 'Off',
+        patientQualificationSuite: studyObject.patientQualificationSuite ? 'On' : 'Off',
+        status: studyObject.status,
+        callTracking: studyObject.callTracking,
+        startDate: studyObject.campaigns[0].dateFrom,
+        endDate: studyObject.campaigns[0].dateTo,
+        level_id: studyObject.campaigns[0].level_id,
+        campaignId: studyObject.campaigns[0].campaignId,
+        campaignlength: studyObject.campaigns[0].length,
+        orderNumber: (index + 1),
+        siteId: studyObject.site.id,
+        campaignLastDate: studyObject.campaignLastDate,
+        unreadMessageCount: studyObject.unreadMessageCount,
+        url: studyObject.url,
+      }));
+      const nEntities = [];
+      _.forEach(entitiesCollection, (item) => {
+        const foundItemIndex = _.findIndex(nEntities, { indication : item.indication, protocol : item.protocol });
+
+        if (foundItemIndex !== -1) {
+          const sItem = nEntities[foundItemIndex];
+          if (!sItem.startDate) {
+            nEntities[foundItemIndex] = item;
+          } else if (sItem.startDate && item.startDate) {
+            const sStartDate = new Date(sItem.startDate);
+            const sEndDate = new Date(sItem.endDate);
+            const nStartDate = new Date(item.startDate);
+            const nEndDate = new Date(item.endDate);
+            if (nStartDate <= cDate && nEndDate >= cDate) {
+              nEntities[foundItemIndex] = item;
+            } else if (sStartDate >= cDate || sEndDate <= cDate) {
+              const sDiff = sStartDate.getTime() - cDate.getTime();
+              const nDiff = nStartDate.getTime() - cDate.getTime();
+              if (sDiff < 0 && nDiff > 0) {
+                nEntities[foundItemIndex] = item;
+              } else if (sDiff < 0 && nDiff < 0) {
+                if (nDiff > sDiff) {
+                  nEntities[foundItemIndex] = item;
+                }
+              } else if (sDiff > 0 && nDiff > 0) {
+                if (nDiff < sDiff) {
+                  nEntities[foundItemIndex] = item;
+                }
+              }
+            }
+          }
+        } else {
+          nEntities.push(item);
+        }
+      });
       return {
         ...state,
         studies: {
-          details: payload.studies,
+          details: nEntities,
           total: payload.total,
           active: payload.active,
           inactive: payload.inactive,
