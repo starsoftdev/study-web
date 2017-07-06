426c49e11e77bddc9d6f23491ddbfa55c20c54c0
ihor <vertical.bitrix@gmail.com>
2017-07-06 20:12:39 +0300

    KIK-1008 Sponsor Portal Calendar feature

---
+++ app/components/SponsorAllEventsModal/index.js
@@ -0,0 +1,73 @@
+import React, { PropTypes } from 'react';
+import { Modal } from 'react-bootstrap';
+import moment from 'moment-timezone';
+import _ from 'lodash';
+
+import CenteredModal from '../CenteredModal';
+
+const AllEventsModal = ({ visible, events, date, handleCloseModal, sortBy, paginationOptions }) => {
+  let sorted = events;
+
+  if (paginationOptions.activeDirection && paginationOptions.activeSort) {
+    const dir = ((paginationOptions.activeDirection === 'down') ? 'desc' : 'asc');
+
+    sorted = _.orderBy(events, [(o) => {
+      if (paginationOptions.activeSort === 'patientName') {
+        return `${o.data.patient.firstName} ${o.data.patient.lastName}`;
+      } else if (paginationOptions.activeSort === 'time') {
+        return o.data.time;
+      }
+      return false;
+    }], [dir]);
+  }
+
+  return (
+    <Modal dialogComponentClass={CenteredModal} show={visible} onHide={handleCloseModal} id="all-patients-modal">
+      <Modal.Header>
+        <Modal.Title>{moment(date).format('dddd, MMMM DD, YYYY')}</Modal.Title>
+        <a className="lightbox-close close" onClick={handleCloseModal}>
+          <i className="icomoon-icon_close" />
+        </a>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="scroll-holder jcf--scrollable">
+          <div className="list-head clearfix">
+            <div onClick={sortBy} data-sort="patientName" className={(paginationOptions.activeSort === 'patientName') ? `${paginationOptions.activeDirection} col patient-name` : 'col patient-name'}>Patient <i className="caret-arrow" /></div>
+            <div className="col site-location hidden">Site Location <i className="caret-arrow" /></div>
+            <div className="col protocol hidden">Protocol <i className="caret-arrow" /></div>
+            <div onClick={sortBy} data-sort="time" className={(paginationOptions.activeSort === 'time') ? `${paginationOptions.activeDirection} col time` : 'col time'}>Time <i className="caret-arrow" /></div>
+          </div>
+          <div className="patient-list">
+            <div className="list-holder">
+              <div className="list-unstyled">
+                {
+                  sorted.map((event, index) => (
+                    <li key={index}>
+                      <a className="btn btn-gray-outline lightbox-opener">
+                        <span className="patient-name">{`Patient #${index + 1}`}</span>
+                        <span className="time">{moment(event.data.time).format('h:mm A')}</span>
+                      </a>
+                    </li>
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+AllEventsModal.propTypes = {
+  visible: PropTypes.bool,
+  events: PropTypes.array,
+  date: PropTypes.object,
+  handleCloseModal: PropTypes.func,
+  handleEdit: PropTypes.func,
+  setAllModalDeferred: PropTypes.func,
+  sortBy: PropTypes.func,
+  paginationOptions: PropTypes.object,
+};
+
+export default AllEventsModal;
---
+++ app/components/SponsorCalendarWidget/index.js
@@ -0,0 +1,99 @@
+/* eslint-disable prefer-template, no-unused-vars */
+
+import React, { PropTypes } from 'react';
+import Calendar from 'react-big-calendar';
+import moment from 'moment-timezone';
+
+import 'react-big-calendar/lib/less/styles.less';
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+Calendar.momentLocalizer(moment); // or globalizeLocalizer
+
+class CalendarWidget extends React.Component {
+  static propTypes = {
+    currentUser: PropTypes.object,
+    sponsorSchedules: PropTypes.array.isRequired,
+    handleOpenModal: PropTypes.func.isRequired,
+    handleShowAll: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.getTimezoneDate = this.getTimezoneDate.bind(this);
+  }
+
+  getTimezoneDate(date) {
+    const { currentUser } = this.props;
+    // we need to compensate for big calendar using a local date offset instead of an international one
+    const offset = moment().local().utcOffset() - moment().tz(currentUser.timezone).utcOffset();
+    let selectedDate;
+    if (offset > 0) {
+      selectedDate = moment(date).add(offset, 'minute');
+    } else if (offset === 0) {
+      selectedDate = moment(date);
+    } else {
+      selectedDate = moment(date).subtract(-offset, 'minute');
+    }
+    return selectedDate;
+  }
+
+  render() {
+    const { currentUser, sponsorSchedules } = this.props;
+    let currDate = null;
+    let counter = 0;
+
+    const eventsList = sponsorSchedules.map(s => {
+      const localTime = moment(s.time);
+      const browserTime = moment()
+        .year(localTime.year())
+        .month(localTime.month())
+        .date(localTime.date())
+        .hour(localTime.hour())
+        .minute(localTime.minute())
+        .seconds(0);
+
+      const result = {
+        data: s,
+        start: browserTime,
+        end: browserTime,
+      };
+
+      if (currDate === null || currDate === moment(s.time).startOf('date').date()) {
+        counter++;
+      } else {
+        counter = 1;
+      }
+
+      result.title = `Patient #${counter} ${moment(s.time).format('h:mm A')}`;
+      currDate = moment(s.time).startOf('date').date();
+      return result;
+    });
+
+    this.currentDate = moment().toDate();
+
+    return (
+      <div className="calendar-box calendar-slider">
+        <Calendar
+          selectable
+          events={eventsList}
+          defaultDate={this.currentDate}
+          culture="en"
+          timezone={currentUser.timezone}
+          onNavigate={(date) => {
+            this.currentDate = date;
+          }}
+          eventPropGetter={(event, start, end, isSelected) => ({
+          })}
+          eventOffset={300}
+          onShowMore={(events, date) => {
+            this.props.handleShowAll(true, events, date);
+          }}
+          ref={(c) => { this.bigCalendar = c; }}
+        />
+      </div>
+    );
+  }
+}
+
+export default CalendarWidget;
