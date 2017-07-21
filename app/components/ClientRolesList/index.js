a474cd7324e2efe17da6f1601fd22f740814062d
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2017-07-21 20:08:46 +0300

    KIK-2273 Site Location, Purchase, and Rewards restriction

--- app/components/ClientRolesList/index.js
+++ app/components/ClientRolesList/index.js
@@ -183,6 +183,7 @@ class ClientRolesList extends Component { // eslint-disable-line react/prefer-st
     let cRedeemable = false;
     let cEditPurchasable = false;
     let cEditRedeemable = false;
+    let editSelf = false;
     if (selectedUser && selectedUser.details && selectedUser.details.roleForClient) {
       if (selectedUser.details.roleForClient.canPurchase || selectedUser.details.roleForClient.canRedeemRewards || selectedUser.details.roleForClient.name === 'Super Admin' || selectedUser.details.roleForClient.name === 'Admin') {
         siteLocation = 0;
@@ -201,6 +202,10 @@ class ClientRolesList extends Component { // eslint-disable-line react/prefer-st
     }
 
     const editUserModalShown = this.editUserModalShouldBeShown();
+    
+    if (selectedUser.details && selectedUser.details.id === currentUser.id) {
+      editSelf = true;
+    }
 
     return (
       <div className="client-roles">
@@ -251,6 +256,7 @@ class ClientRolesList extends Component { // eslint-disable-line react/prefer-st
                   <div className="form-lightbox">
                     <EditUserForm
                       initialValues={selectedUserDetailsForForm}
+                      editSelf={editSelf}
                       siteOptions={siteOptions}
                       deleting={deletedClientRole.deleting}
                       onDelete={this.deleteClientRole}
