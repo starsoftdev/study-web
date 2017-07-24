9778f4e30c66dc93142540c4cacc1c471c6ce97a
ydagana <jens.cpp@gmail.com>
2017-07-24 22:59:26 +0100

    KIK-1610, making sure mixpanel token is defined

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -106,7 +106,7 @@ module.exports = (options) => ({
       },
       API_URL: JSON.stringify(process.env.API_URL),
       SOCKET_URL: JSON.stringify(process.env.SOCKET_URL),
-      MIXPANEL_TOKEN: JSON.stringify(process.env.MIXPANEL_TOKEN),
+      MIXPANEL_TOKEN: JSON.stringify(process.env.MIXPANEL_TOKEN || false),
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
   ]),
