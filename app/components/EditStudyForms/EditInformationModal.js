6761ad1b7deb882f3cf72a47a46ed3ed03ba739f
Mike Li <mike@instataskapp.com>
2017-08-24 14:47:39 -0700

    Fix editing study modal to be more modular.

--- app/components/EditStudyForms/EditInformationModal.js
+++ app/components/EditStudyForms/EditInformationModal.js
@@ -4,22 +4,41 @@
 
 import React, { PropTypes } from 'react';
 import Collapse from 'react-bootstrap/lib/Collapse';
+import { normalizePhoneDisplay } from '../../common/helper/functions';
+
 import EditInformationForm from './EditInformationForm';
 
 export default class EditInformationModal extends React.Component {
   static propTypes = {
     addEmailNotificationClick: PropTypes.func.isRequired,
-    initialValues: PropTypes.object.isRequired,
+    study: PropTypes.object,
     onClose: PropTypes.func.isRequired,
     openModal: PropTypes.bool.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.getEditStudyInitialValues = this.getEditStudyInitialValues.bind(this);
+  }
+
   componentDidMount() {
   }
 
+  getEditStudyInitialValues(study) {
+    const initialValues = Object.assign({}, study);
+    initialValues.recruitment_phone = normalizePhoneDisplay(initialValues.recruitment_phone);
+    initialValues.site = study.site_id;
+    delete initialValues.site_id;
+    initialValues.messagingNumber = study.text_number_id;
+    delete initialValues.text_number_id;
+
+    return initialValues;
+  }
+
   renderModalContent() {
-    const { addEmailNotificationClick, initialValues, openModal, onClose } = this.props;
+    const { addEmailNotificationClick, openModal, onClose, study } = this.props;
     if (openModal) {
+      const initialValues = this.getEditStudyInitialValues(study);
       return (
         <div>
           <div className="form-area">
