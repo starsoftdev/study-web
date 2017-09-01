c9f4e6b1e20fcff2d42ab5c9083f72824edd68bd
Dmitry Nessonov <dnessonov@gmail.com>
2017-09-01 14:45:58 +0300

    KIK-2623 client portal email blast UI for study page

---
+++ app/components/EmailBlastForm/validator.js
@@ -0,0 +1,8 @@
+import { validatorFactory } from '../../utils/reduxForm';
+
+const schema = {
+  message: { presence: { message: '^Message is required.' } },
+  patients: { presence: { message: '^Error! No patients selected.' } },
+};
+
+export default validatorFactory(schema);
