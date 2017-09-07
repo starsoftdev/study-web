6856106589f9bda180954a57534353ead7025276
Mike Li <mike@instataskapp.com>
2017-09-07 15:02:08 -0700

    Fix actions and various saga related logic for tagged indications.

--- app/components/EditStudyForms/reducer.js
+++ app/components/EditStudyForms/reducer.js
@@ -10,23 +10,25 @@ import {
   REMOVE_STUDY_INDICATION_TAG_SUCCESS,
 } from '../../containers/HomePage/AdminDashboard/constants';
 
-export default function editDashboardStudyReducer(state, action) {
-  switch (action) {
+const initialState = {};
+
+export default function editDashboardStudyReducer(state = initialState, action) {
+  switch (action.type) {
     case ADD_STUDY_INDICATION_TAG_SUCCESS: {
       let taggedIndicationsForStudy;
       if (state.values.taggedIndicationsForStudy) {
         taggedIndicationsForStudy = [
           ...state.values.taggedIndicationsForStudy,
           {
-            label: action.indication.label,
-            value: action.indication.value,
+            label: action.indication.name,
+            value: action.indication.id,
           },
         ];
       } else {
         taggedIndicationsForStudy = [
           {
-            label: action.indication.label,
-            value: action.indication.value,
+            label: action.indication.name,
+            value: action.indication.id,
           },
         ];
       }
@@ -39,7 +41,6 @@ export default function editDashboardStudyReducer(state, action) {
       };
     }
     case REMOVE_STUDY_INDICATION_TAG_SUCCESS:
-      console.log(state.values.taggedIndicationsForStudy);
       return {
         ...state,
         values: {
