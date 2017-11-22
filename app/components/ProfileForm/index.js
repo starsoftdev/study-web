d6ed39be4572eba1b0c7fe4ef91bdde7b097cdec
Jay Chen <iagilelite@gmail.com>
2017-11-23 04:56:29 +0900

    KIK-3021 refactor related page

--- app/components/ProfileForm/index.js
+++ app/components/ProfileForm/index.js
@@ -31,6 +31,8 @@ const toShortCode = country => {
       return 'fr';
     case 'Germany':
       return 'de';
+    case 'Hungary':
+      return 'hu';
     case 'Italy':
       return 'it';
     case 'Czech Republic':
