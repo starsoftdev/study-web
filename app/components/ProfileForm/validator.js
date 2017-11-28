834336f123bcc2ab40a8e92c71577dd87770d96b
Mike Li <mike@instataskapp.com>
2017-11-27 21:02:21 -0800

    Revert "KIK-3049, Modify the user setup flow to be able to redisplay the setup popup"
    This reverts commit 1014dc149714275eb83e43457ec650b7509bff0c.

--- app/components/ProfileForm/validator.js
+++
@@ -1,9 +0,0 @@
-import { validatorFactory } from '../../utils/reduxForm';
-
-const schema = {
-  address: { presence: true },
-};
-const fields = Object.keys(schema);
-
-export { fields };
-export default validatorFactory(schema);
