b7f16e40f3a39e1ab8e4c1e5fa5059b14ef16e0e
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2017-12-01 16:57:53 +0300

    KIK-3040 patient locks for the study page and patient database page(revert changes)

--- app/containers/PatientDatabasePage/reducer.js
+++ app/containers/PatientDatabasePage/reducer.js
@@ -18,7 +18,6 @@ import {
   SAVE_PATIENT,
   SAVE_PATIENT_SUCCESS,
   SAVE_PATIENT_ERROR,
-  SAVE_PATIENT_LOCKED,
 
   INIT_CHAT,
   DISABLE_CHAT,
@@ -365,15 +364,6 @@ export default function patientDatabasePageReducer(state = initialState, action)
         selectedPatient: {
           details: null,
           fetching: false,
-          error: null,
-        },
-      };
-    case SAVE_PATIENT_LOCKED:
-      return {
-        ...state,
-        savedPatient: {
-          details: null,
-          saving: false,
           error: null,
         },
       };
