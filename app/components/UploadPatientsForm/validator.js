84cf320c45906e4c763bfed8024d5a978d232186
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2017-12-20 22:16:12 +0200

    Fix lint errors after merge.

--- app/components/UploadPatientsForm/validator.js
+++ app/components/UploadPatientsForm/validator.js
@@ -13,24 +13,6 @@ const schema = {
   protocol: {
     presence: true,
   },
-  /*groupname: {
-    presence: {
-      message: '^Error! Name field is required.',
-    },
-  },
-  groupemail: {
-    presence: {
-      message: '^Error! Email field is required.',
-    },
-  },
-  groupphone: {
-    presence: {
-      message: '^Error! Phone field is required.',
-    },
-  },
-  groupage: { presence: false },
-  groupgender: { presence: false },
-  groupbmi: { presence: false },*/
 };
 
 const fields = Object.keys(schema);
