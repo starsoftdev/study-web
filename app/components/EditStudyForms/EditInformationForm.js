44437c3efdca6f9f6126dbcea4361dcf8ec6d8ae
Mike Li <mike@instataskapp.com>
2017-12-20 11:13:50 -0800

    KIK-3221 Disables site location dropdown because there are a number of bugs caused by site locations switched otherwise

--- app/components/EditStudyForms/EditInformationForm.js
+++ app/components/EditStudyForms/EditInformationForm.js
@@ -383,6 +383,7 @@ export default class EditInformationForm extends React.Component {
                   clearable={false}
                   backspaceRemoves={false}
                   deleteRemoves={false}
+                  isDisabled
                 />
               </div>
             </div>
