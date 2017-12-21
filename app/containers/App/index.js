d76ebae44699c6a00bde93fbd8bfb681c94e87f6
ydagana <jens.cpp@gmail.com>
2017-12-21 18:33:04 +0000

    KIK-3145, binding this to function

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -58,6 +58,7 @@ class App extends React.Component { // eslint-disable-line react/prefer-stateles
     this.stayLoggedIn = this.stayLoggedIn.bind(this);
     this.changePassword = this.props.changePassword.bind(this);
     this.handleCloseEmailModal = this.handleCloseEmailModal.bind(this);
+    this.handleCloseBulkUploadModal = this.handleCloseBulkUploadModal.bind(this);
     this.state = {
       forceLogout: parseInt(FORCE_LOGOUT), // should be 10 hours in milliseconds
       timeout: parseInt(IDLE_TIMEOUT), // should be 2 hours in milliseconds
