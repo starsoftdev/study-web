28724db38ed675a0d5538cf3703b34b65dc0df0d
ydagana <jens.cpp@gmail.com>
2018-01-12 14:28:47 +0000

    attempt to fix the build error on travis

--- install.js
+++ install.js
@@ -2,10 +2,14 @@
  * Created by mike on 6/20/16.
  */
 
-const Validate = require('git-validate');
-if (Validate) {
+try {
+  const Validate = require('git-validate'); // eslint-disable-line
+
   Validate.configureHook('pre-commit#develop', ['lint:fix']);
   Validate.installHooks('pre-commit#develop');
   Validate.configureHook('pre-commit#master', ['lint:fix']);
   Validate.installHooks('pre-commit#master');
+} catch (err) {
+  console.error(err);
 }
+
