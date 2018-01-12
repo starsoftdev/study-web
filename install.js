6d8ec5e067839a2ba00f7ddf48dd4dd501bd2628
ydagana <jens.cpp@gmail.com>
2018-01-12 14:20:36 +0000

    attempt to fix issue with travis

--- install.js
+++ install.js
@@ -3,8 +3,9 @@
  */
 
 const Validate = require('git-validate');
-
-Validate.configureHook('pre-commit#develop', ['lint:fix']);
-Validate.installHooks('pre-commit#develop');
-Validate.configureHook('pre-commit#master', ['lint:fix']);
-Validate.installHooks('pre-commit#master');
+if (Validate) {
+  Validate.configureHook('pre-commit#develop', ['lint:fix']);
+  Validate.installHooks('pre-commit#develop');
+  Validate.configureHook('pre-commit#master', ['lint:fix']);
+  Validate.installHooks('pre-commit#master');
+}
