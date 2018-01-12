76b7b16495616f3436bb97a6744b17b4cc545245
ydagana <jens.cpp@gmail.com>
2018-01-12 14:12:38 +0000

    KIK-3266, fix issue where cns value is not sent to the API when it's empty

--- app/components/ThankYouPageModal/index.js
+++ app/components/ThankYouPageModal/index.js
@@ -132,6 +132,7 @@ export class ThankYouPageModal extends React.Component {
     }
 
     const list = Object.assign({ studyId: this.state.selected.study_id }, newList);
+    list.cns = newList.cns || null;
     submitForm(list);
   }
 
