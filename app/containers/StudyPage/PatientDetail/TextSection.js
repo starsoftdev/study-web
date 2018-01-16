143f463fe231359e43b7ce354a959e85ca78af85
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-01-16 08:20:36 -0800

    KIK-3249 implement realtime study stats update

--- app/containers/StudyPage/PatientDetail/TextSection.js
+++ app/containers/StudyPage/PatientDetail/TextSection.js
@@ -160,7 +160,12 @@ class TextSection extends React.Component {
         });
         this.props.studyStatsFetched({
           ...this.props.studyStats,
-          textsSent: this.props.studyStats.textsSent + 1,
+          total: this.props.studyStats.texts + 1,
+          sent: this.props.studyStats.textsSent + 1,
+          received: this.props.studyStats.textsReceived,
+          totalDuration: this.props.studyStats.callsDuration,
+          views: this.props.studyStats.views,
+          countReceived: this.props.studyStats.calls,
         });
         this.setState({ enteredCharactersLength: 0 }, () => {
           textarea.value = '';
