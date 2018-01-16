acb868d23dd535193113be0ac296e79b2c28258a
Mike Li <mike@instataskapp.com>
2018-01-15 17:39:23 -0800

    Revert "Revert "KIK-3220 fix height""
    This reverts commit 29540bdf298de6184b29e79f1a6ff58bb2f06575.

---
+++ app/containers/DashboardLockedUsers/tests/selectors.test.js
@@ -0,0 +1,10 @@
+// import { selectDashboardLockedUsersDomain } from '../selectors';
+import expect from 'expect';
+
+// const selector = selectDashboardLockedUsersDomain();
+
+describe('selectDashboardLockedUsersDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect('Test case').toEqual(false);
+  });
+});
