e5306fbae6f145fd6a0365d99ef758c6da9d001a
Mike Li <mike@instataskapp.com>
2018-01-17 15:40:12 -0800

    Revert KIK-3249
    This reverts commit 239eeda3a4ec451d051d36615dd637b84a74c316.

--- app/containers/StudyPage/PatientDetail/TextSection.js
+++ app/containers/StudyPage/PatientDetail/TextSection.js
@@ -8,7 +8,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { reduxForm } from 'redux-form';
 import { toastr } from 'react-redux-toastr';
-import { readStudyPatientMessages, updatePatientSuccess, studyStatsFetched } from '../actions';
+import { readStudyPatientMessages, updatePatientSuccess } from '../actions';
 import CallItem from '../../../components/GlobalPMSModal/CallItem';
 import { markAsReadPatientMessages, deleteMessagesCountStat } from '../../App/actions';
 import * as Selector from '../selectors';
@@ -43,11 +43,9 @@ class TextSection extends React.Component {
     markAsReadPatientMessages: React.PropTypes.func,
     deleteMessagesCountStat: React.PropTypes.func,
     updatePatientSuccess: React.PropTypes.func,
-    studyStatsFetched: React.PropTypes.func,
     ePMS: React.PropTypes.bool,
     currentPatientCategory: React.PropTypes.object,
     site: React.PropTypes.object,
-    studyStats: React.PropTypes.object,
   };
 
   constructor(props) {
@@ -157,15 +155,6 @@ class TextSection extends React.Component {
         this.props.updatePatientSuccess(currentPatient.id, currentPatientCategory.id, {
           lastTextMessage: { body: data.body, dateCreated: data.dateCreated },
           updatedAt: data.dateCreated,
-        });
-        this.props.studyStatsFetched({
-          ...this.props.studyStats,
-          total: this.props.studyStats.texts + 1,
-          sent: this.props.studyStats.textsSent + 1,
-          received: this.props.studyStats.textsReceived,
-          totalDuration: this.props.studyStats.callsDuration,
-          views: this.props.studyStats.views,
-          countReceived: this.props.studyStats.calls,
         });
         this.setState({ enteredCharactersLength: 0 }, () => {
           textarea.value = '';
@@ -293,7 +282,6 @@ const mapStateToProps = createStructuredSelector({
   clientCredits: selectClientCredits(),
   currentPatientCategory: Selector.selectCurrentPatientCategory(),
   site: Selector.selectSite(),
-  studyStats: Selector.selectStudyStats(),
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -304,7 +292,6 @@ const mapDispatchToProps = (dispatch) => ({
   markAsReadPatientMessages: (patientId) => dispatch(markAsReadPatientMessages(patientId)),
   deleteMessagesCountStat: (payload) => dispatch(deleteMessagesCountStat(payload)),
   updatePatientSuccess: (patientId, patientCategoryId, payload) => dispatch(updatePatientSuccess(patientId, patientCategoryId, payload)),
-  studyStatsFetched: (payload) => dispatch(studyStatsFetched(payload)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TextSection);
