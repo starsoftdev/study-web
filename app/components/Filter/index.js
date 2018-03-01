a994e8a0291223e42c7ff2461ea2ca948d14faed
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-03-01 10:56:52 -0800

    Revert KIK-3573
    This reverts commit 8d46c21621bbd1aacff79c970909c685226f8afb.

--- app/components/Filter/index.js
+++ app/components/Filter/index.js
@@ -16,14 +16,13 @@ export default class Filter extends React.Component {
     onClose: React.PropTypes.func,
     onChange: React.PropTypes.func,
     onSubmit: React.PropTypes.func,
-    oldSearchValue: React.PropTypes.number,
   };
 
   componentDidMount() {
   }
 
   submitSearch(value) {
-    if (!value || !/^\d+$/.test(value) || value > 2147483647) {
+    if (!value || !/^\d+$/.test(value)) {
       toastr.error('', 'Error! Invalid study number.');
       return;
     }
@@ -56,26 +55,9 @@ export default class Filter extends React.Component {
       >
         <strong className="title">Search</strong>
         <input
-          type="number" name={name} className="form-control" placeholder="Study #"
-          onKeyDown={() => {
-            this.setState({ oldSearchValue: this.searchVal.value });
-          }}
-          onKeyUp={() => {
-            if (!parseInt(this.searchVal.value) || this.searchVal.value > 2147483647) {
-              if (this.state.oldSearchValue < 2147483647) {
-                this.searchVal.value = this.state.oldSearchValue;
-              } else {
-                let oldValue = this.state.oldSearchValue;
-                while (oldValue > 2147483647) {
-                  oldValue = parseInt(oldValue / 10);
-                }
-                this.searchVal.value = oldValue;
-              }
-            }
-          }}
-          ref={(searchVal) => (
-            this.searchVal = searchVal
-          )}
+          type="text" name={name} className="form-control" placeholder="Study #" ref={(searchVal) => (
+          this.searchVal = searchVal
+        )}
         />
         <button className="btn btn-default" onClick={() => this.submitSearch(this.searchVal.value)}>Apply</button>
         <a className="btn-close" onClick={() => this.props.onClose()}>
