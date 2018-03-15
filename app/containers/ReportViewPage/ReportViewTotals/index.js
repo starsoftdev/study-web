4a7d6ad31a067a36e08ac1798818f81119fb092a
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-03-15 22:19:24 +0200

    KIK-3847. Add loading indicator to Sponsor Portal stats.

--- app/containers/ReportViewPage/ReportViewTotals/index.js
+++ app/containers/ReportViewPage/ReportViewTotals/index.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import LoadingSpinner from '../../../components/LoadingSpinner';
 
 export class ReportViewTotals extends React.Component { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
@@ -25,40 +26,43 @@ export class ReportViewTotals extends React.Component { // eslint-disable-line r
     const percentage = this.props.getPercentageObject(totals);
 
     return (
-      <ul className="list-inline list-stats">
-        <li>
-          <strong className="heading"><span>NOT<br /> CONTACTED</span></strong>
-          <strong className="number"><span>{totals.count_not_contacted}<span className="small">{`(${percentage.count_not_contacted_p}%)`}</span></span></strong>
-        </li>
-        <li>
-          <strong className="heading"><span>CALL<br /> ATTEMPTED</span></strong>
-          <strong className="number"><span>{totals.call_attempted}<span className="small">{`(${percentage.call_attempted_p}%)`}</span></span></strong>
-        </li>
-        <li onClick={() => { this.props.openNotesModal(null, 'Not Qualified / Not Interested', 'DNQ'); }}>
-          <strong className="heading"><span>DNQ</span></strong>
-          <strong className="number pointer"><span>{totals.dnq}<span className="small">{`(${percentage.dnq_p}%)`}</span></span></strong>
-        </li>
-        <li onClick={() => { this.props.openNotesModal(null, 'Action Needed', 'ACTION NEEDED'); }}>
-          <strong className="heading"><span>ACTION NEEDED</span></strong>
-          <strong className="number pointer"><span>{totals.action_needed}<span className="small">{`(${percentage.action_needed_p}%)`}</span></span></strong>
-        </li>
-        <li>
-          <strong className="heading"><span>SCHEDULED</span></strong>
-          <strong className="number"><span>{totals.scheduled}<span className="small">{`(${percentage.scheduled_p}%)`}</span></span></strong>
-        </li>
-        <li>
-          <strong className="heading"><span>CONSENTED</span></strong>
-          <strong className="number"><span>{totals.consented}<span className="small">{`(${percentage.consented_p}%)`}</span></span></strong>
-        </li>
-        <li onClick={() => { this.props.openNotesModal(null, 'Screen Failed', 'SCREEN FAILED'); }}>
-          <strong className="heading"><span>SCREEN<br /> FAILED</span></strong>
-          <strong className="number pointer"><span>{totals.screen_failed}<span className="small">{`(${percentage.screen_failed_p}%)`}</span></span></strong>
-        </li>
-        <li>
-          <strong className="heading"><span>RANDOMIZED</span></strong>
-          <strong className="number"><span>{totals.randomized}<span className="small">{`(${percentage.randomized_p}%)`}</span></span></strong>
-        </li>
-      </ul>
+      <div className="report-page-totals-container">
+        {this.props.totals.fetching && <div className="text-center report-page-total-loading-container"><LoadingSpinner showOnlyIcon /></div>}
+        <ul className="list-inline list-stats">
+          <li>
+            <strong className="heading"><span>NOT<br /> CONTACTED</span></strong>
+            <strong className="number"><span>{totals.count_not_contacted}<span className="small">{`(${percentage.count_not_contacted_p}%)`}</span></span></strong>
+          </li>
+          <li>
+            <strong className="heading"><span>CALL<br /> ATTEMPTED</span></strong>
+            <strong className="number"><span>{totals.call_attempted}<span className="small">{`(${percentage.call_attempted_p}%)`}</span></span></strong>
+          </li>
+          <li onClick={() => { this.props.openNotesModal(null, 'Not Qualified / Not Interested', 'DNQ'); }}>
+            <strong className="heading"><span>DNQ</span></strong>
+            <strong className="number pointer"><span>{totals.dnq}<span className="small">{`(${percentage.dnq_p}%)`}</span></span></strong>
+          </li>
+          <li onClick={() => { this.props.openNotesModal(null, 'Action Needed', 'ACTION NEEDED'); }}>
+            <strong className="heading"><span>ACTION NEEDED</span></strong>
+            <strong className="number pointer"><span>{totals.action_needed}<span className="small">{`(${percentage.action_needed_p}%)`}</span></span></strong>
+          </li>
+          <li>
+            <strong className="heading"><span>SCHEDULED</span></strong>
+            <strong className="number"><span>{totals.scheduled}<span className="small">{`(${percentage.scheduled_p}%)`}</span></span></strong>
+          </li>
+          <li>
+            <strong className="heading"><span>CONSENTED</span></strong>
+            <strong className="number"><span>{totals.consented}<span className="small">{`(${percentage.consented_p}%)`}</span></span></strong>
+          </li>
+          <li onClick={() => { this.props.openNotesModal(null, 'Screen Failed', 'SCREEN FAILED'); }}>
+            <strong className="heading"><span>SCREEN<br /> FAILED</span></strong>
+            <strong className="number pointer"><span>{totals.screen_failed}<span className="small">{`(${percentage.screen_failed_p}%)`}</span></span></strong>
+          </li>
+          <li>
+            <strong className="heading"><span>RANDOMIZED</span></strong>
+            <strong className="number"><span>{totals.randomized}<span className="small">{`(${percentage.randomized_p}%)`}</span></span></strong>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
