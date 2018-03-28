da14e3bb6518c2e25ee131e330870e5965eacee7
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-03-28 12:56:10 +0300

    KIK-3865. Fix media tracking dashboard slider bugs.

--- app/components/CallTrackingPageModal/validator.js
+++ app/components/CallTrackingPageModal/validator.js
@@ -15,25 +15,33 @@ export default values => {
       // check for unique messagingNumber
       let isUnique = true;
       let isGoogleUrlUnique = true;
+      let isNameUnique = true;
       values.leadSource.forEach((leadInner, indexInner) => {
         if (indexInner !== index && leadInner.messagingNumber && lead.messagingNumber && leadInner.messagingNumber.value && lead.messagingNumber.value && leadInner.messagingNumber.value === lead.messagingNumber.value) {
           isUnique = false;
         }
-        if (indexInner !== index && leadInner.googleUrl && lead.googleUrl && leadInner.googleUrl === lead.googleUrl) {
+        if (indexInner !== index && leadInner.url && lead.url && leadInner.url === lead.url) {
           isGoogleUrlUnique = false;
+        }
+        if (indexInner !== index && leadInner.source_name && lead.source_name && leadInner.source_name === lead.source_name) {
+          isNameUnique = false;
         }
       });
       let messagingNumberErrors = null;
       let googleUrlErrors = null;
+      let sourceNameErrors = null;
       if (!isUnique) {
         messagingNumberErrors = { messagingNumber : ['Messaging Number should be unique'] };
       }
       if (!isGoogleUrlUnique) {
-        googleUrlErrors = { googleUrl : ['Google Url should be unique'] };
+        googleUrlErrors = { url : ['Url should be unique'] };
+      }
+      if (!isNameUnique) {
+        sourceNameErrors = { source_name : ['Source name should be unique'] };
       }
 
-      if (!lead.source || !lead.source_name || messagingNumberErrors || googleUrlErrors) {
-        leadSourceErrors[index] = { ...leadError, ...messagingNumberErrors, ...googleUrlErrors };
+      if (!lead.source || !lead.source_name || messagingNumberErrors || googleUrlErrors || sourceNameErrors) {
+        leadSourceErrors[index] = { ...leadError, ...messagingNumberErrors, ...googleUrlErrors, ...sourceNameErrors };
       }
     });
   }
