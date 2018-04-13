7e5e485441477895ce57a3fd8f78c469dbbca894
Younes Dagana <jens.cpp@gmail.com>
2018-04-13 14:30:25 +0100

    ENG-83, add postal code validation when countryCode is us

--- corporate/components/ClinicalTrialsSearchForm/validator.js
+++ corporate/components/ClinicalTrialsSearchForm/validator.js
@@ -1,7 +1,18 @@
 import { validatorFactory } from '../../../app/utils/reduxForm';
 
 const schema = {
-  postalCode: { presence: false },
+  postalCode: {
+    presence: false,
+    format: (value, attributes) => {
+      if (attributes.countryCode && attributes.countryCode !== 'us') {
+        return null;
+      }
+      if ((/^\d{5}(?:[-\s]\d{4})?$/).test(value)) {
+        return null;
+      }
+      return 'Invalid zip code';
+    },
+  },
   distance: { presence: false },
   indication_id: { presence: false },
 };
