853954d94e4e23be90be4145616e04e57b3c241f
Hideki Kagawa <jherosk@gmail.com>
2018-04-28 02:18:40 +0900

    ACQ-99 call center homepage

---
+++ app/containers/CallCenterHomePage/CallCalendar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './style.less';
+
+export default class CallCalendar extends React.Component {
+  render() {
+    return (
+      <div className="ccCalendar">
+        <h3>Calendar</h3>
+        <div className="ccCal-box">
+          Agenda
+        </div>
+        <div className="ccCal-box">
+          Agenda
+        </div>
+        <div className="ccCal-box">
+          Agenda
+        </div>
+        <div className="ccCal-box">
+          Agenda
+        </div>
+      </div>
+    );
+  }
+}
