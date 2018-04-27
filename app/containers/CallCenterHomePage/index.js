56090bcae9b48ff8bd290f1f585a1a5bac5ca5eb
Hideki Kagawa <jherosk@gmail.com>
2018-04-28 03:17:49 +0900

    quick comment fix

--- app/containers/CallCenterHomePage/index.js
+++ app/containers/CallCenterHomePage/index.js
@@ -1,12 +1,6 @@
 /**
- * NotFoundPage
+ * Call Center Homepage
  *
- * This is the page we show when the user visits a url that doesn't have a route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a neccessity for you then you can refactor it and remove
- * the linting exception.
  */
 
 import React from 'react';
