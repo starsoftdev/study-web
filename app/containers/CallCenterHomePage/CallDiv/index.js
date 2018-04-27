1dd1da068cfeb6df177f8229d9a4a8252e002940
Hideki Kagawa <jherosk@gmail.com>
2018-04-28 02:18:40 +0900

    ACQ-99 call center homepage

---
+++ app/containers/CallCenterHomePage/CallDiv/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+import './style.less';
+
+export default class CallDiv extends React.Component {
+  render() {
+    return (
+      <div className="cc-container">
+        <div className="cc-row">
+          <div className="cc-box cc-box-heading">
+            New Patient
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+        </div>
+        <div className="cc-row">
+          <div className="cc-box cc-box-heading">
+            Call1
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+        </div>
+        <div className="cc-row">
+          <div className="cc-box cc-box-heading">
+            Call2
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+        </div>
+        <div className="cc-row">
+          <div className="cc-box cc-box-heading">
+            Call3
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+          </div>
+        </div>
+        <div className="cc-row">
+          <div className="cc-box cc-box-heading">
+            Meetings
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+            <span>Meeting Time</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+            <span>Meeting Time</span>
+          </div>
+          <div className="cc-box">
+            <span>Patient Name</span>
+            <span>Indication</span>
+            <span>Meeting Time</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
