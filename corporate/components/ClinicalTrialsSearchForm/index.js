a48ee43b7bc70b5ee36e051735a7bc74cb8e909e
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-05-08 01:09:10 -0700

    ACQ-82 add australia

--- corporate/components/ClinicalTrialsSearchForm/index.js
+++ corporate/components/ClinicalTrialsSearchForm/index.js
@@ -83,6 +83,10 @@ export class ClinicalTrialsSearchForm extends React.Component { // eslint-disabl
 
     const countries = [
       {
+        name: translate('common.countryName.au'),
+        id: 'au',
+      },
+      {
         name: translate('common.countryName.br'),
         id: 'br',
       },
