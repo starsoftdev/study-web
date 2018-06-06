d9b6c08d4a01111663e26a69da7da98929193140
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-07 01:24:24 +0900

    adding additional render functions for call2, call3 and meetings

--- app/containers/CallCenterHomePage/CallDiv/index.js
+++ app/containers/CallCenterHomePage/CallDiv/index.js
@@ -41,6 +41,29 @@ export default class CallDiv extends React.Component {
     return output;
   }
 
+  renderCall2 = () => {
+    return null;
+  }
+
+  renderCall3 = () => {
+    return null;
+  }
+
+  renderMeetings = () => {
+    const { patients, indications } = this.props;
+
+    const output = [];
+    _.forEach(patients.details, (patient) => {
+      if (patient && patient.patient_category_id === 5) {
+        output.push(<div className="cc-box" key={`callDiv_call1_${patient.id}`}>
+          <span>{patient.first_name} {patient.last_name}</span>
+          <span>{indications[patient.study_id].name}</span>
+        </div>);
+      }
+    });
+    return output;
+  }
+
   render() {
     return (
       <div className="cc-container">
@@ -60,55 +83,19 @@ export default class CallDiv extends React.Component {
           <div className="cc-box cc-box-heading">
             {translate('container.page.callcenter.heading.call')}2
           </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-          </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-          </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-          </div>
+          { this.renderCall2() }
         </div>
         <div className="cc-row">
           <div className="cc-box cc-box-heading">
             {translate('container.page.callcenter.heading.call')}3
           </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-          </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-          </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-          </div>
+          { this.renderCall3() }
         </div>
         <div className="cc-row">
           <div className="cc-box cc-box-heading">
             {translate('container.page.callcenter.heading.meetings')}
           </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-            <span>Meeting Time</span>
-          </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-            <span>Meeting Time</span>
-          </div>
-          <div className="cc-box">
-            <span>Patient Name</span>
-            <span>Indication</span>
-            <span>Meeting Time</span>
-          </div>
+          { this.renderMeetings() }
         </div>
       </div>
     );
