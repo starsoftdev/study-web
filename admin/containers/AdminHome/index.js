1b8b21b88d751f2423ac725b8dbc16a66dca75eb
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-06 13:48:49 +0300

    ENG-208 create a new study info section (Layout)

--- admin/containers/AdminHome/index.js
+++ admin/containers/AdminHome/index.js
@@ -14,6 +14,7 @@ import ExpandableSection from '../../components/ExpandableSection';
 import FiltersPageForm from '../../components/FiltersPageForm';
 import MediaStatsTable from '../../components/MediaStatsTable';
 import FilterQueryForm from '../../components/Filter/FilterQueryForm';
+import StudyInfo from '../../components/StudyInfo';
 import { selectFilterFormValues } from './selectors';
 const formName = 'adminDashboardFilters';
 
@@ -142,6 +143,7 @@ export class AdminHome extends Component { // eslint-disable-line react/prefer-s
         <div id="mediaStatsBox">
           <ExpandableSection content={<MediaStatsTable />} />
         </div>
+        <StudyInfo />
       </div>
     );
   }
