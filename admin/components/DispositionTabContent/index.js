64d0c75b729addddf6fdd7f5a47f38c6151e1212
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-07 13:19:11 +0300

    ENG-224 create a "DISPOSITION" tab (Layout)

--- admin/components/DispositionTabContent/index.js
+++ admin/components/DispositionTabContent/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
 
-export default class ReportTabTable extends React.Component { // eslint-disable-line react/prefer-stateless-function
+export default class DispositionTabContent extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
     return (
       <table>
         <tr className="header">
-          <th>Study #</th>
-          <th>Site Location</th>
-          <th>Site Address</th>
-          <th>Exposure Level</th>
-          <th>Start Date</th>
-          <th>End Date</th>
-          <th>New Patient</th>
-          <th>Call Attempted</th>
-          <th>DNQ / Not Interested</th>
-          <th>Action Needed</th>
-          <th>Scheduled</th>
-          <th>Consented</th>
-          <th>Screen Failed</th>
-          <th>Randomized</th>
-          <th>Total</th>
+          <th>STUDY #</th>
+          <th>SITE LOCATION</th>
+          <th>SITE ADDRESS</th>
+          <th>EXPOSURE LEVEL</th>
+          <th>START DATE</th>
+          <th>END DATE</th>
+          <th>PASSED PRESCREENER</th>
+          <th>DID NOT PASS PRESCREENER</th>
+          <th>NOT INTERESTED</th>
+          <th>ALL ATTEMPTS MADE</th>
+          <th>TOTAL</th>
         </tr>
         <tr>
           <th>1000001 - 3</th>
