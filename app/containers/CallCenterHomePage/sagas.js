f71f177928dae45492be6a5ca1a00ebc05d4e07c
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-11 10:23:12 +0200

    ACQ-132 load patient schedules into cc homepage

--- app/containers/CallCenterHomePage/sagas.js
+++ app/containers/CallCenterHomePage/sagas.js
@@ -1,14 +1,21 @@
-import { take, call, put, fork, cancel } from 'redux-saga/effects';
+import { take, takeLatest, call, put, fork, cancel } from 'redux-saga/effects';
 import { LOCATION_CHANGE } from 'react-router-redux';
+import { toastr } from 'react-redux-toastr';
+import { get } from 'lodash';
+
 import request from '../../utils/request';
+import { removeItem } from '../../utils/localStorage';
 
 import {
   FETCH_PATIENTS,
+  FETCH_SCHEDULES,
 } from './constants';
 
 import {
   patientsFetched,
   patientsFetchingError,
+  schedulesFetched,
+  schedulesFetchingError,
 } from './actions';
 
 export function* fetchPatientsWatcher() {
@@ -42,12 +49,34 @@ export function* fetchPatientsWatcher() {
   }
 }
 
+export function* fetchSchedulesWatcher() {
+  while (true) {
+    yield take(FETCH_SCHEDULES);
+    try {
+      const requestURL = `${API_URL}/callCenterAppointments/getAppointmentsForUser`;
+      const response = yield call(request, requestURL);
+
+      yield put(schedulesFetched(response));
+    } catch (err) {
+      const errorMessage = get(err, 'message', 'Something went wrong while fetching schedules.');
+      toastr.error('', errorMessage);
+      yield put(schedulesFetchingError(err));
+      if (err.status === 401) {
+        removeItem('auth_token');
+        yield call(() => { location.href = '/login'; });
+      }
+    }
+  }
+}
+
 export function* callCenterHomePageSaga() {
   const watcherA = yield fork(fetchPatientsWatcher);
+  const watcherB = yield fork(fetchSchedulesWatcher);
 
   yield take(LOCATION_CHANGE);
 
   yield cancel(watcherA);
+  yield cancel(watcherB);
 }
 
 // Bootstrap sagas
