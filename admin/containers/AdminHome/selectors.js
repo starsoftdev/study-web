53547d230df7e5f4d236ad7aace24576a3a1a0ca
Younes Dagana <jens.cpp@gmail.com>
2018-06-16 11:43:31 +0000

    ENG-402, creating studies loading functionality for new admin panel

--- admin/containers/AdminHome/selectors.js
+++ admin/containers/AdminHome/selectors.js
@@ -1,7 +1,7 @@
 import { createSelector } from 'reselect';
 import { get } from 'lodash';
 
-const selectAdminHomeDomain = () => state => state.adminHome;
+const selectAdminHomeDomain = () => state => state;
 const selectFormDomain = () => state => state.form;
 
 const selectAdminHomePage = () => createSelector(
@@ -14,7 +14,13 @@ const selectFilterFormValues = () => createSelector(
   substate => get(substate, 'adminDashboardFilters.values', {})
 );
 
+const selectStudies = () => createSelector(
+  selectAdminHomeDomain(),
+  substate => substate.studies
+);
+
 export default selectAdminHomePage;
 export {
   selectFilterFormValues,
+  selectStudies,
 };
