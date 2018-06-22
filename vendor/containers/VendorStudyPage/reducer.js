4ece299001e75e02c0042ff36defa09d4283eeca
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-22 20:34:17 +0300

    ACQ-251. Vendor Portal - Setup Infrastructure.

---
+++ vendor/containers/VendorStudyPage/reducer.js
@@ -0,0 +1,11 @@
+/* eslint-disable comma-dangle, no-case-declarations */
+
+const initialState = {
+};
+
+export default function vendorStudyPageReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
