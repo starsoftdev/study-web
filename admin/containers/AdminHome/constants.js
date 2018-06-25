ef3c8f8944a110ea4a55347b8a600aaa1100e379
Younes Dagana <jens.cpp@gmail.com>
2018-06-25 21:09:43 +0100

    ENG-240, restructuring removing unused code in the new admin panel

--- admin/containers/AdminHome/constants.js
+++ admin/containers/AdminHome/constants.js
@@ -1,16 +1,2 @@
 /* eslint-disable no-multi-spaces */
 
-export const ADD_CUSTOM_FILTER                          = 'AdminHomePage/ADD_CUSTOMçFILTER';
-export const REMOVE_CUSTOM_FILTER                       = 'AdminHomePage/REMOVE_CUSTOM_FILTER';
-export const CLEAR_CUSTOM_FILTERS                       = 'AdminHomePage/CLEAR_CUSTOM_FILTERS';
-export const CLEAR_FILTERS                              = 'AdminHomePage/CLEAR_FILTERS';
-
-export const CLEAR_STUDIES                              = 'AdminHomePage/CLEAR_STUDIES';
-
-export const FETCH_STUDIES_FOR_ADMIN                    = 'AdminHomePage/FETCH_STUDIES_FOR_ADMIN';
-export const FETCH_STUDIES_FOR_ADMIN_SUCCESS            = 'AdminHomePage/FETCH_STUDIES_FOR_ADMIN_SUCCESS';
-export const FETCH_STUDIES_FOR_ADMIN_ERROR              = 'AdminHomePage/FETCH_STUDIES_FOR_ADMIN_ERROR';
-
-export const FETCH_TOTALS_FOR_ADMIN                     = 'AdminHomePage/FETCH_TOTALS_FOR_ADMIN';
-export const FETCH_TOTALS_FOR_ADMIN_SUCCESS             = 'AdminHomePage/FETCH_TOTALS_FOR_ADMIN_SUCCESS';
-export const FETCH_TOTALS_FOR_ADMIN_ERROR               = 'AdminHomePage/FETCH_TOTALS_FOR_ADMIN_ERROR';
--- admin/containers/AdminHome/selectors.js
+++ admin/containers/AdminHome/selectors.js
@@ -1,32 +1,10 @@
 import { createSelector } from 'reselect';
-import { get } from 'lodash';
 
 const selectAdminHomeDomain = () => state => state.adminHomePage;
-const selectFormDomain = () => state => state.form;
 
 const selectAdminHomePage = () => createSelector(
   selectAdminHomeDomain(),
   substate => substate
-);
-
-const selectFilterFormValues = () => createSelector(
-  selectFormDomain(),
-  substate => get(substate, 'adminDashboardFilters.values', {})
-);
-
-const selectStudies = () => createSelector(
-  selectAdminHomeDomain(),
-  substate => substate.studies
-);
-
-const selectTotals = () => createSelector(
-  selectAdminHomeDomain(),
-  substate => substate.totals
-);
-
-const selectCustomFilters = () => createSelector(
-  selectAdminHomeDomain(),
-  substate => substate.customFilters
 );
 
 const selectPaginationOptions = () => createSelector(
@@ -36,9 +14,5 @@ const selectPaginationOptions = () => createSelector(
 
 export default selectAdminHomePage;
 export {
-  selectFilterFormValues,
-  selectStudies,
-  selectTotals,
-  selectCustomFilters,
   selectPaginationOptions,
 };
