441695659c180cc8b166a02568b6b053da49c705
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-06-26 12:50:14 +0300

    ENG-238 create Patient Category Table (Functionality)

--- admin/containers/AdminHome/actions.js
+++ admin/containers/AdminHome/actions.js
@@ -5,6 +5,7 @@
 import {
   FETCH_STUDIES_FOR_ADMIN, FETCH_STUDIES_FOR_ADMIN_SUCCESS, FETCH_STUDIES_FOR_ADMIN_ERROR,
   FETCH_TOTALS_FOR_ADMIN, FETCH_TOTALS_FOR_ADMIN_SUCCESS, FETCH_TOTALS_FOR_ADMIN_ERROR,
+  FETCH_MEDIA_TOTALS_FOR_ADMIN, FETCH_MEDIA_TOTALS_FOR_ADMIN_SUCCESS, FETCH_MEDIA_TOTALS_FOR_ADMIN_ERROR,
   CLEAR_FILTERS, ADD_CUSTOM_FILTER, REMOVE_CUSTOM_FILTER, CLEAR_CUSTOM_FILTERS, CLEAR_STUDIES,
 } from './constants';
 
@@ -60,6 +61,27 @@ export function fetchTotalsForAdminSuccess(payload) {
 export function fetchTotalsForAdminError(payload) {
   return {
     type: FETCH_TOTALS_FOR_ADMIN_ERROR,
+    payload,
+  };
+}
+
+export function fetchMediaTotalsForAdmin(params) {
+  return {
+    type: FETCH_MEDIA_TOTALS_FOR_ADMIN,
+    params,
+  };
+}
+
+export function fetchMediaTotalsForAdminSuccess(payload) {
+  return {
+    type: FETCH_MEDIA_TOTALS_FOR_ADMIN_SUCCESS,
+    payload,
+  };
+}
+
+export function fetchMediaTotalsForAdminError(payload) {
+  return {
+    type: FETCH_MEDIA_TOTALS_FOR_ADMIN_ERROR,
     payload,
   };
 }
