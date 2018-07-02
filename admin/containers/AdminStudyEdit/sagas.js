7a7b05377c444a093eecfcbe8bb1b222f5a3e556
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-07-02 06:22:44 -0400

    ENG-375 create a "NOTES" section in the Study Edit page (Functionality)

--- admin/containers/AdminStudyEdit/sagas.js
+++ admin/containers/AdminStudyEdit/sagas.js
@@ -1,3 +1,116 @@
+import { takeLatest } from 'redux-saga';
+import { call, put, fork, cancel, take } from 'redux-saga/effects';
+import { get } from 'lodash';
+import { toastr } from 'react-redux-toastr';
+import { LOCATION_CHANGE } from 'react-router-redux';
+
+import request from '../../utils/request';
+
+import {
+  FETCH_NOTE,
+  ADD_NOTE,
+  DELETE_NOTE,
+} from './constants';
+
+import {
+  fetchNoteSuccess,
+  fetchNoteError,
+  addNoteSuccess,
+  addNoteError,
+  deleteNoteSuccess,
+  deleteNoteError,
+} from './actions';
+
 // Bootstrap sagas
 export default [
+  adminStudyEditSaga,
 ];
+
+export function* fetchNoteWatcher() {
+  yield* takeLatest(FETCH_NOTE, fetchNoteWorker);
+}
+
+export function* fetchNoteWorker(action) {
+  try {
+    const requestURL = `${API_URL}/notes`;
+    const { studyId } = action;
+
+    const filterObj = {
+      where: { study_id: studyId },
+      include: [{
+        relation: 'user',
+        scope: {
+          where: { isArchived: false },
+          include: 'roleForClient',
+        },
+      }],
+    };
+
+    const queryParams = {
+      filter: JSON.stringify(filterObj),
+    };
+
+    const response = yield call(request, requestURL, { query: queryParams });
+
+    yield put(fetchNoteSuccess(response));
+  } catch (err) {
+    const errorMessage = get(err, 'message', 'Something went wrong while fetching note');
+    toastr.error('', errorMessage);
+    yield put(fetchNoteError(err));
+  }
+}
+
+export function* addNoteWatcher() {
+  yield* takeLatest(ADD_NOTE, addNoteWorker);
+}
+
+export function* addNoteWorker(action) {
+  try {
+    const requestURL = `${API_URL}/notes`;
+
+    const params = {
+      method: 'POST',
+      body: JSON.stringify(action.payload),
+    };
+    const response = yield call(request, requestURL, params);
+
+    yield put(addNoteSuccess(response));
+  } catch (err) {
+    const errorMessage = get(err, 'message', 'Something went wrong while saving note');
+    toastr.error('', errorMessage);
+    yield put(addNoteError(err));
+  }
+}
+
+export function* deleteNoteWatcher() {
+  yield* takeLatest(DELETE_NOTE, deleteNoteWorker);
+}
+
+export function* deleteNoteWorker(action) {
+  try {
+    const requestURL = `${API_URL}/notes/${action.payload}`;
+
+    const params = {
+      method: 'DELETE',
+    };
+    yield call(request, requestURL, params);
+
+    yield put(deleteNoteSuccess({ id: action.payload }));
+  } catch (err) {
+    const errorMessage = get(err, 'message', 'Something went wrong while deleting note');
+    toastr.error('', errorMessage);
+    yield put(deleteNoteError(err));
+  }
+}
+
+export function* adminStudyEditSaga() {
+  const fetchNoteWatcher1 = yield fork(fetchNoteWatcher);
+  const addNoteWatcher1 = yield fork(addNoteWatcher);
+  const deleteNoteWatcher1 = yield fork(deleteNoteWatcher);
+
+
+  yield take(LOCATION_CHANGE);
+  yield cancel(fetchNoteWatcher1);
+  yield cancel(addNoteWatcher1);
+  yield cancel(deleteNoteWatcher1);
+}
