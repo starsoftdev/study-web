7791ce8cb6483878350e99f39428bc9a82ece7e3
Younes Dagana <jens.cpp@gmail.com>
2018-07-03 15:45:35 +0100

    ENG-370, layout for media tracking tab

--- admin/containers/AdminStudyEdit/actions.js
+++ admin/containers/AdminStudyEdit/actions.js
@@ -14,6 +14,12 @@ import {
   DELETE_NOTE,
   DELETE_NOTE_SUCCESS,
   DELETE_NOTE_ERROR,
+  DELETE_MEDIA_TYPE,
+  DELETE_MEDIA_TYPE_SUCCESS,
+
+  EDIT_MEDIA_TYPES,
+  EDIT_MEDIA_TYPES_ERROR,
+  EDIT_MEDIA_TYPES_SUCCESS,
 } from './constants';
 
 export function fetchNote(studyId) {
@@ -78,3 +84,46 @@ export function deleteNoteError(payload) {
     payload,
   };
 }
+
+export function deleteMediaType(studyId, studySourceId, index) {
+  return {
+    type: DELETE_MEDIA_TYPE,
+    studyId,
+    studySourceId,
+    index,
+  };
+}
+
+export function deleteMediaTypeSuccess(index) {
+  return {
+    type: DELETE_MEDIA_TYPE_SUCCESS,
+    index,
+  };
+}
+
+
+export function editMediaTypes(studyId, mediaTypes, mediaTracking) {
+  return {
+    type: EDIT_MEDIA_TYPES,
+    studyId,
+    mediaTypes,
+    mediaTracking,
+  };
+}
+
+export function editMediaTypesSuccess(mediaTypes, studyId, mediaTracking) {
+  return {
+    type: EDIT_MEDIA_TYPES_SUCCESS,
+    mediaTypes,
+    studyId,
+    mediaTracking,
+  };
+}
+
+export function editMediaTypesError(payload) {
+  return {
+    type: EDIT_MEDIA_TYPES_ERROR,
+    payload,
+  };
+}
+
