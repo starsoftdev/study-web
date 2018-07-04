57437b438db8bd3bdb74dc4cf6d18666b72195b4
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-07-04 21:45:18 +0300

    ENG-374. Admin Portal - Create a Study Edit page.

---
+++ admin/components/StudyInfoSection/RenderEmailsList.js
@@ -0,0 +1,114 @@
+import React, { PropTypes } from 'react';
+import { Field } from 'redux-form';
+import { forEach, filter } from 'lodash';
+
+import Checkbox from '../../components/Input/Checkbox';
+
+export default class RenderEmailsList extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  static propTypes = {
+    change: PropTypes.func.isRequired,
+    formValues: PropTypes.object.isRequired,
+    fields: PropTypes.object,
+    addEmailNotificationClick: PropTypes.func,
+    closeEmailNotification: PropTypes.func,
+    emailFields: PropTypes.array,
+    currentUser: PropTypes.object,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.addEmailNotificationClick = this.addEmailNotificationClick.bind(this);
+    this.selectAll = this.selectAll.bind(this);
+    this.selectEmail = this.selectEmail.bind(this);
+  }
+
+  componentDidMount() {
+  }
+
+  addEmailNotificationClick() {
+    const { addEmailNotificationClick, currentUser } = this.props;
+    if (currentUser && currentUser.roleForClient.isAdmin) {
+      addEmailNotificationClick();
+    }
+  }
+
+  selectAll(e) {
+    const { change, formValues } = this.props;
+    if (formValues.emailNotifications) {
+      forEach(formValues.emailNotifications, (value, index) => {
+        change(`emailNotifications[${index}].isChecked`, e);
+      });
+    }
+  }
+
+  selectEmail(e) {
+    const { change, formValues } = this.props;
+    if (formValues.checkAllInput && !e) {
+      change('checkAllInput', false);
+    } else if (!formValues.checkAllInput && e) {
+      const checkedArr = filter(formValues.emailNotifications, (o) => o.isChecked);
+      if ((checkedArr.length + 1) === formValues.emailNotifications.length) {
+        change('checkAllInput', true);
+      }
+    }
+  }
+
+  renderEmailList(email, index) {
+    const { formValues } = this.props;
+    return (
+      <li key={index}>
+        <Field
+          name={`${email}.isChecked`}
+          component={Checkbox}
+          type="checkbox"
+          className="field-active"
+          onChange={this.selectEmail}
+        />
+        <span className="email">{formValues.emailNotifications[index].email} </span>
+      </li>
+    );
+  }
+
+  render() {
+    const { fields, formValues, currentUser } = this.props;
+    const enableAddEmailNotif = currentUser && currentUser.roleForClient.isAdmin;
+    let formValuesLength;
+    if (formValues.emailNotifications) {
+      formValuesLength = formValues.emailNotifications.length;
+    } else {
+      formValuesLength = 0;
+    }
+    if (fields.length !== formValuesLength && fields.length !== 0) {
+      return (
+        <div></div>
+      );
+    }
+    return (
+      <div>
+        <div className={fields.length === 0 ? 'heading-area-no-border' : 'heading-area'}>
+          <Field
+            name="checkAllInput"
+            component={Checkbox}
+            type="checkbox"
+            className="field-active"
+            onChange={this.selectAll}
+          />
+          <strong className="email">RECEIVE EMAIL NOTIFICATION</strong>
+        </div>
+
+        <ul className="list-unstyled list-emails">
+          {
+            fields.map((emailNotification, index) => {
+              return this.renderEmailList(emailNotification, index);
+            })
+          }
+        </ul>
+        <div className="btn-holder">
+          <a className="add-new-email lightbox-opener" onClick={this.addEmailNotificationClick} disabled={!enableAddEmailNotif}>Add Email Notification</a>
+        </div>
+      </div>
+    );
+  }
+}
--- admin/components/StudyInfoSection/index.js
+++ admin/components/StudyInfoSection/index.js
@@ -1,10 +1,34 @@
 import React, { Component, PropTypes } from 'react';
 import Button from 'react-bootstrap/lib/Button';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { arrayRemoveAll, arrayPush, blur, change, Field, FieldArray, reduxForm, startSubmit, stopSubmit } from 'redux-form';
+import Toggle from '../../components/Input/Toggle';
+import Input from '../../components/Input/index';
+import ReactSelect from '../../components/Input/ReactSelect';
+import RenderEmailsList from './RenderEmailsList';
+
+import { selectStudyInfo, selectIndications, selectProtocols, selectSponsors, selectCro, selectSiteLocations, selectUsersByRoles, selectMessagingNumbers } from  '../../containers/AdminStudyEdit/selectors';
 import Checkbox from '../Input/Checkbox';
 
+const formName = 'Admin.EditStudyForm';
+import formValidator from './validator';
+
+@reduxForm({
+  form: formName,
+  validate: formValidator,
+})
+@connect(mapStateToProps, mapDispatchToProps)
 export class StudyInfoSection extends Component { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
-    study: PropTypes.object,
+    studyId: PropTypes.string,
+    studyInfo: PropTypes.object,
+    change: PropTypes.func.isRequired,
+    indications: PropTypes.array.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    siteLocations: PropTypes.array.isRequired,
+    usersByRoles: PropTypes.object.isRequired,
+    messagingNumbers: PropTypes.object.isRequired,
   };
 
   constructor() {
@@ -50,44 +74,282 @@ export class StudyInfoSection extends Component { // eslint-disable-line react/p
 
   render() {
     const { userEmailNotif, emailNotif } = this.state;
+    const { change, indications, sponsors, protocols, cro, siteLocations, usersByRoles, messagingNumbers } = this.props;
+
+    const indicationsOptions = indications.map(item => ({ value: item.id, label: item.name }));
+    const sponsorsOptions = sponsors.map(item => ({ value: item.id, label: item.name }));
+    const protocolsOptions = protocols.map(item => ({ value: item.id, label: item.number }));
+    const croOptions = cro.map(item => ({ value: item.id, label: item.name }));
+    const siteLocationsOptions = siteLocations.map(item => ({ value: item.id, label: item.location }));
+    const smOptions = usersByRoles.sm.map(item => ({ value: item.id, label: `${item.first_name} ${item.last_name}` }));
+    const bdOptions = usersByRoles.bd.map(item => ({ value: item.id, label: `${item.first_name} ${item.last_name}` }));
+    const ccOptions = usersByRoles.cc.map(item => ({ value: item.id, label: `${item.first_name} ${item.last_name}` }));
+
+    const messagingNumbersOptions = messagingNumbers.details.map(item => ({
+      value: item.id,
+      label: item.phone_number,
+    }));
+    if (this.props.studyInfo.text_number_id) {
+      messagingNumbersOptions.unshift({
+        value: this.props.studyInfo.text_number_id,
+        label: this.props.studyInfo.phone_number,
+      });
+    }
 
     return (
-      <div id="studyInfoSection">
-        <h2>{this.props.study.id}</h2>
+      <form id="studyInfoSection" onSubmit={this.props.handleSubmit}>
+        <h2>{this.props.studyId}</h2>
         <ul className="section">
-          <li>STATUS: </li>
-          <li>STUDY URL: </li>
-          <li>INDICATION: </li>
-          <li>PROTOCOL: </li>
-          <li>SPONSOR: </li>
-          <li>CRO: </li>
+          <li>
+            <div className="field-row">
+              <strong className="label">
+                <label htmlFor="new-patient-first-name">STATUS:</label>
+              </strong>
+              <div className="field text-right">
+                <Field
+                  name="isPublic"
+                  component={Toggle}
+                  className="field"
+                  onChange={(e) => {
+                    change('isPublic', e);
+                  }}
+                />
+              </div>
+            </div>
+          </li>
+          <li><div className="field-row">
+            <strong className="label required">
+              <label htmlFor="new-patient-first-name">STUDY URL:</label>
+            </strong>
+            <div className="field">
+              <Field
+                type="text"
+                id="edit-information-page-name"
+                name="landingPageUrl"
+                component={Input}
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">INDICATION:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="indication_id"
+                component={ReactSelect}
+                placeholder="Select Indication"
+                searchPlaceholder="Search"
+                searchable
+                clearable={false}
+                backspaceRemoves={false}
+                deleteRemoves={false}
+                options={indicationsOptions}
+                customSearchIconClass="icomoon-icon_search2"
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">PROTOCOL:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="protocol_id"
+                component={ReactSelect}
+                placeholder="Select Protocol"
+                searchPlaceholder="Search"
+                searchable
+                clearable={false}
+                backspaceRemoves={false}
+                deleteRemoves={false}
+                options={protocolsOptions}
+                customSearchIconClass="icomoon-icon_search2"
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">SPONSOR:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="sponsor_id"
+                component={ReactSelect}
+                placeholder="Select Sponsor"
+                searchPlaceholder="Search"
+                searchable
+                options={sponsorsOptions}
+                customSearchIconClass="icomoon-icon_search2"
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">CRO:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="cro_id"
+                component={ReactSelect}
+                placeholder="Select CRO"
+                searchPlaceholder="Search"
+                searchable
+                options={croOptions}
+                customSearchIconClass="icomoon-icon_search2"
+                onChange={(e) => {
+                  change('croId', e ? e.toString() : null);
+                }}
+              />
+            </div>
+          </div></li>
         </ul>
         <ul className="section">
-          <li>SITE LOCATION: </li>
-          <li>SITE NUMBER: </li>
-          <li>SITE ADDRESS: </li>
-          <li>RECRUITMENT PHONE: </li>
-          <li>PRINCIPAL INVESTIGATOR: </li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">SITE LOCATION:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="site"
+                component={ReactSelect}
+                placeholder="Select Site Location"
+                searchPlaceholder="Search"
+                searchable
+                options={siteLocationsOptions}
+                onChange={(e) => {
+                  this.siteLocationChanged(e);
+                }}
+                customSearchIconClass="icomoon-icon_search2"
+                clearable={false}
+                backspaceRemoves={false}
+                deleteRemoves={false}
+                disabled
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-email">SITE NUMBER:</label>
+            </strong>
+            <div className="field">
+              <Field
+                type="text"
+                name="site"
+                component={Input}
+                isDisabled
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">SITE ADDRESS:</label>
+            </strong>
+            <div className="field">
+              <Field
+                type="text"
+                name="site_address"
+                component={Input}
+
+                isDisabled
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">RECRUITMENT PHONE:</label>
+            </strong>
+            <div className="field">
+              <Field
+                type="text"
+                name="recruitment_phone"
+                component={Input}
+                onBlur={this.onPhoneBlur}
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label>PRINCIPAL INVESTIGATOR:</label>
+            </strong>
+            <div className="field">
+              <Field
+                type="text"
+                name="piName"
+                component={Input}
+                placeholder="Full Name"
+              />
+            </div>
+          </div></li>
         </ul>
         <ul className="section">
-          <li>AO: </li>
-          <li>BD: </li>
-          <li>CC: </li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">AO:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="sm_user_id"
+                component={ReactSelect}
+                placeholder="Select Ad Operation"
+                searchPlaceholder="Search"
+                searchable
+                options={smOptions}
+                customSearchIconClass="icomoon-icon_search2"
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">BD:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="bd_user_id"
+                component={ReactSelect}
+                placeholder="Select Business Development"
+                searchPlaceholder="Search"
+                searchable
+                options={bdOptions}
+                customSearchIconClass="icomoon-icon_search2"
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">CC:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="cc_user_id"
+                component={ReactSelect}
+                placeholder="Select Call Center"
+                searchPlaceholder="Search"
+                searchable
+                options={ccOptions}
+                customSearchIconClass="icomoon-icon_search2"
+              />
+            </div>
+          </div></li>
         </ul>
         <div className="section">
           <div className="smallSection">
             <h3>USER EMAIL NOTIFICATION</h3>
             <ul>
-              {
-                userEmailNotif.map((e, i) => {
-                  return (
-                    <li key={i} className="hasCheckbox">
-                      <Checkbox name="userEmailNotif" input={{ checked: e.checked, onChange: (v) => this.onCheckboxClick('userEmailNotif', i, v) }} />
-                      {e.email}
-                    </li>
-                  );
-                })
-              }
+              <div className="field-row">
+                <strong className="label"><label>USER EMAIL NOTIFICATIONS</label></strong>
+                <div className="field">
+                  <div className="emails-list-holder">
+                    {<FieldArray
+                      name="emailNotifications"
+                      component={RenderEmailsList}
+                      change={change}
+                      formValues={formValues}
+                      addEmailNotificationClick={addEmailNotificationClick}
+                    />}
+                  </div>
+                </div>
+              </div>
             </ul>
             <Button bsStyle="primary" type="submit">Add</Button>
           </div>
@@ -108,23 +370,104 @@ export class StudyInfoSection extends Component { // eslint-disable-line react/p
             <Button bsStyle="primary" type="submit">Add</Button>
           </div>
         </div>
-        <div className="updateBtnSection">
-          <Button type="submit">Update</Button>
-        </div>
         <ul className="section">
-          <li>SPONSOR PORTAL: </li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-first-name">SPONSOR PORTAL:</label>
+            </strong>
+            <div className="field text-right">
+              <Field
+                name="shouldShowInSponsorPortal"
+                component={Toggle}
+                className="field"
+                onChange={(e) => {
+                  change('shouldShowInSponsorPortal', e);
+                }}
+              />
+            </div>
+          </div></li>
           <li>CENTRAL: </li>
-          <li>PMS: </li>
-          <li>DELETE PATIENT: </li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-first-name">PMS:</label>
+            </strong>
+            <div className="field text-right">
+              <Field
+                name="patientMessagingSuite"
+                component={Toggle}
+                className="field"
+                onChange={(e) => {
+                  change('patientMessagingSuite', e);
+                }}
+              />
+            </div>
+          </div></li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">DELETE PATIENT:</label>
+            </strong>
+            <div className="field text-right">
+              <Field
+                name="canDeletePatient"
+                component={Toggle}
+                className="field"
+                onChange={(e) => {
+                  if (e === false) change('canDeletePatient', e.toString());
+                }}
+              />
+            </div>
+          </div></li>
         </ul>
         <ul className="section">
-          <li>MESSAGING NUMBER: </li>
+          <li><div className="field-row">
+            <strong className="label">
+              <label htmlFor="new-patient-phone">MESSAGING NUMBER:</label>
+            </strong>
+            <div className="field">
+              <Field
+                name="messagingNumber"
+                component={ReactSelect}
+                placeholder="Select Messaging Number"
+                searchPlaceholder="Search"
+                searchable
+                options={messagingNumbersOptions}
+                customSearchIconClass="icomoon-icon_search2"
+                onChange={(messagingNumberId) => {
+                  if (messagingNumberId) {
+                    change('messagingNumber', parseInt(messagingNumberId));
+                  }
+                }}
+              />
+            </div>
+          </div></li>
           <li>FACEBOOK URL: </li>
           <li>CNS CODE: </li>
         </ul>
-      </div>
+        <div className="btn-block text-right">
+          <button type="submit" className="btn btn-default btn-add-row">
+            <span>Update</span>
+          </button>
+        </div>
+      </form>
     );
   }
 }
 
-export default StudyInfoSection;
+function mapDispatchToProps(dispatch) {
+  return {
+    change: (field, value) => dispatch(change(formName, field, value)),
+  };
+}
+
+const mapStateToProps = createStructuredSelector({
+  studyInfo: selectStudyInfo(),
+  indications: selectIndications(),
+  sponsors: selectSponsors(),
+  protocols: selectProtocols(),
+  cro: selectCro(),
+  siteLocations: selectSiteLocations(),
+  usersByRoles: selectUsersByRoles(),
+  messagingNumbers: selectMessagingNumbers(),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StudyInfoSection);
