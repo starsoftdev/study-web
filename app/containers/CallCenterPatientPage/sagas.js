328bb08720b45b9910b26fcb34234dfe3a2ddb8b
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-07-05 11:35:57 +0300

    ACQ-300. Unread message notifications remain on the home page after the text has been read.

--- app/containers/CallCenterPatientPage/sagas.js
+++ app/containers/CallCenterPatientPage/sagas.js
@@ -17,6 +17,7 @@ import {
   SUBMIT_DELETE_NOTE,
   SUBMIT_EMAIL,
   SUBMIT_PATIENT_DISPOSITION,
+  READ_STUDY_PATIENT_MESSAGES,
 } from './constants';
 
 import {
@@ -30,6 +31,8 @@ import {
   updatePatientSuccess,
   patientDispositionSubmitted,
   patientDispositionSubmissionError,
+  readStudyPatientMessagesSuccess,
+  readStudyPatientMessagesError,
 } from './actions';
 
 import {
@@ -347,6 +350,30 @@ function* sendPatientMessagesWorker(action) {
   }
 }
 
+function* readStudyPatientMessagesWatcher() {
+  yield* takeLatest(READ_STUDY_PATIENT_MESSAGES, readStudyPatientMessagesWorker);
+}
+
+function* readStudyPatientMessagesWorker(action) {
+  try {
+    const { patientId } = action;
+    if (patientId && patientId > 0) {
+      const requestURL = `${API_URL}/patients/${patientId}/markMessagesAsRead`;
+      const response = yield call(request, requestURL);
+
+      yield put(readStudyPatientMessagesSuccess(response));
+    } else {
+      yield put(readStudyPatientMessagesSuccess([]));
+    }
+  } catch (err) {
+    yield put(readStudyPatientMessagesError(err));
+    if (err.status === 401) {
+      removeItem('auth_token');
+      yield call(() => { location.href = '/login'; });
+    }
+  }
+}
+
 export function* callCenterPatientPageSaga() {
   try {
     const watcherA = yield fork(fetchPatientWatcher);
@@ -357,6 +384,7 @@ export function* callCenterPatientPageSaga() {
     const watcherF = yield fork(submitPatientUpdate);
     const watcherG = yield fork(submitPatientDisposition);
     const watcherH = yield fork(sendPatientMessagesWatcher);
+    const watcherI = yield fork(readStudyPatientMessagesWatcher);
 
     yield take(LOCATION_CHANGE);
 
@@ -368,6 +396,7 @@ export function* callCenterPatientPageSaga() {
     yield cancel(watcherF);
     yield cancel(watcherG);
     yield cancel(watcherH);
+    yield cancel(watcherI);
   } catch (e) {
     // if returns forbidden we remove the token from local storage
     if (e.status === 401) {
