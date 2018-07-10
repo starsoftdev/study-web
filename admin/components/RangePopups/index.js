c9e01c167a70609ad130521436a4d58d3341d75d
ihor <vertical.bitrix@gmail.com>
2018-07-10 12:13:42 -0400

    ENG-388 create functionality for "DOWNLOAD" for campaign period(fix merge issue)

--- admin/components/RangePopups/index.js
+++ admin/components/RangePopups/index.js
@@ -38,6 +38,10 @@ export default class RangePopups extends Component {
         startDate: null,
         endDate: null,
       },
+      endDateRange: {
+        startDate: null,
+        endDate: null,
+      },
     };
 
     this.showPopup = this.showPopup.bind(this);
@@ -160,6 +164,8 @@ export default class RangePopups extends Component {
   }
 
   render() {
+    const { endDateRange } = this.state;
+    const endDateRangeLabel = (endDateRange.startDate && endDateRange.endDate) ? `${endDateRange.startDate} - ${endDateRange.endDate}` : 'Study End Date Range';
     return (
       <div id="btnsPopupsHolder">
         <div className="col pull-right no-right-padding">
@@ -169,7 +175,7 @@ export default class RangePopups extends Component {
         </div>
         <div className="col pull-right">
           <button type="button" className="btn btn-primary pull-right" onClick={(ev) => this.showPopup(ev, 'studyEndDateRange')}>
-            Study End Date Range
+            {endDateRangeLabel}
           </button>
         </div>
         <div className="col pull-right">
