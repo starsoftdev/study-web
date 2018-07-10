4139c82c953a11ebfb044a0f2ccaad3bda9e9b77
Mike Li <mike.li@studykik.com>
2018-07-10 14:25:45 -0700

    ACQ-240
    Adds additional validation for the forms.

--- vendor/containers/Admin/EditVendorStudiesForm/index.js
+++ vendor/containers/Admin/EditVendorStudiesForm/index.js
@@ -1,22 +1,28 @@
-import React, { PropTypes } from 'react';
 import _ from 'lodash';
+import React, { PropTypes } from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import Form from 'react-bootstrap/lib/Form';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 import { Field, reduxForm } from 'redux-form';
+import { createStructuredSelector } from 'reselect';
 import Input from '../../../../app/components/Input';
 import LoadingSpinner from '../../../../app/components/LoadingSpinner';
 import { translate } from '../../../../common/utilities/localization';
 import { addStudyNumber, deleteStudyNumber, fetchVendorStudies } from './actions';
 import { selectStudiesForVendor } from './selectors';
-import { selectAsyncErrorBool, selectAsyncValidatingBool, selectValues } from '../../../../common/selectors/form.selector';
+import {
+  selectAnyTouchedBool,
+  selectAsyncErrorBool,
+  selectAsyncValidatingBool,
+  selectValues,
+} from '../../../../common/selectors/form.selector';
 import validator from './validator';
 import { asyncValidate } from './asyncValidate';
 
 export const formName = 'VendorAdminPage.EditVendorStudiesForm';
 
 const mapStateToProps = createStructuredSelector({
+  anyTouched: selectAnyTouchedBool(formName),
   asyncValidating: selectAsyncValidatingBool(formName),
   asyncError: selectAsyncErrorBool(formName),
   values: selectValues(formName),
@@ -29,16 +35,17 @@ const mapDispatchToProps = {
 };
 
 @reduxForm({
-  form: formName,
-  validate: validator,
   asyncValidate,
   asyncBlurFields: ['studyId'],
+  form: formName,
+  validate: validator,
   shouldAsyncValidate: ({ trigger }) => trigger !== 'submit',
 })
 @connect(mapStateToProps, mapDispatchToProps)
 export default class EditVendorStudiesForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
     addStudyNumber: PropTypes.func.isRequired,
+    anyTouched: PropTypes.bool.isRequired,
     asyncValidating: PropTypes.bool.isRequired,
     asyncError: PropTypes.bool.isRequired,
     deleteStudyNumber: PropTypes.func.isRequired,
@@ -102,7 +109,7 @@ export default class EditVendorStudiesForm extends React.Component { // eslint-d
 
   render() {
 
-    const { handleSubmit, saving } = this.props;
+    const { anyTouched, asyncError, asyncValidating, handleSubmit, saving } = this.props;
 
     return (
       <Form className="form-lightbox dashboard-lightbox study-number-form" onSubmit={handleSubmit}>
@@ -133,7 +140,7 @@ export default class EditVendorStudiesForm extends React.Component { // eslint-d
         </div>
 
         <div className="field-row text-right no-margins">
-          <Button bsStyle="primary" type="submit">
+          <Button bsStyle="primary" type="submit" disabled={!anyTouched || asyncValidating || !asyncError}>
             {saving ?
               <span>
                 <LoadingSpinner showOnlyIcon size={20} className="saving-user" />
