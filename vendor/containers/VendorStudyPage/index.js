90d5e5c6e908c2bf82fa3047c58d2bb9fff31cab
ngkhoawork <26078620+ngkhoawork@users.noreply.github.com>
2018-07-25 23:20:20 -0300

    Client Portal: Add 'All' option to 'Select Media' dropdown

--- vendor/containers/VendorStudyPage/index.js
+++ vendor/containers/VendorStudyPage/index.js
@@ -127,7 +127,7 @@ export default class StudyPage extends React.Component { // eslint-disable-line 
   componentWillMount() {
     const { params, setStudyId, fetchStudy, fetchPatientCategories, socket, clientOpenedStudyPage, fetchStudySources } = this.props;
     setStudyId(parseInt(params.id));
-    fetchStudy(params.id, 1);     // fetch STUDYKIK source by default = 1
+    fetchStudy(params.id);
     fetchPatientCategories(params.id);
     fetchStudySources(params.id);
     if (socket && socket.connected) {
@@ -269,7 +269,7 @@ export default class StudyPage extends React.Component { // eslint-disable-line 
 
   handleSubmit(searchFilter, loadMore) {
     const { params: { id }, paginationOptions } = this.props;
-    const sourceId = searchFilter.sourceId || (searchFilter.source !== '') ? searchFilter.source : 1;
+    const sourceId = searchFilter.source;
     const campaignId = searchFilter.campaignId || searchFilter.campaign;
     let skip = 0;
     if (loadMore) {
@@ -305,7 +305,7 @@ export default class StudyPage extends React.Component { // eslint-disable-line 
       };
     });
     campaignOptions.unshift({ label: translate('common.constants.all'), value: -1 });
-    let defaultSource = '';
+    let defaultSource;
     const sourceOptions = this.props.studySources.details.filter(s => !s.isMediaType).map(studySource => {
       if (studySource.source.type === 'StudyKIK') {
         defaultSource = studySource.source.value;
@@ -315,6 +315,7 @@ export default class StudyPage extends React.Component { // eslint-disable-line 
         value: studySource.source.value,
       };
     });
+    sourceOptions.unshift({ label: translate('common.constants.all'), value: -1 });
     const siteLocation = site.name;
     let sponsor = 'None';
     if (study.sponsor) {
