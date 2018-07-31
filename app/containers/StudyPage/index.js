95639d03db01e04f85cdeb87c60230a05af6a90b
jherosk <jherosk@gmail.com>
2018-08-01 00:45:15 +0900

    ACQ-357 fix for campaign filtering

--- app/containers/StudyPage/index.js
+++ app/containers/StudyPage/index.js
@@ -267,7 +267,7 @@ export class StudyPage extends React.Component { // eslint-disable-line react/pr
         value: campaign.id,
       };
     });
-    campaignOptions.unshift({ label: translate('common.constants.all'), value: null });
+    campaignOptions.unshift({ label: translate('common.constants.all'), value: -1 });
     let defaultSource = '';
     const sourceOptions = this.props.studySources.details.filter(s => !s.isMediaType).map(studySource => {
       if (studySource.source.type === 'StudyKIK') {
